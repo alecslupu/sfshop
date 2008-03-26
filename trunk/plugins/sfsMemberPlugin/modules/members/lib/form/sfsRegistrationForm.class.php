42e2c59f2fa720bd2a669235588a72c555e8279a
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-03-26 07:00:25 +0000

    added class for creating registration form, added template for registration

---
+++ trunk/plugins/sfsMemberPlugin/modules/members/lib/form/sfsRegistrationForm.class.php
@@ -0,0 +1,86 @@
+<?php
+class sfsRegistrationForm extends sfsMemberForm
+{
+    public function configure()
+    {
+        $arrayQuestions = array();
+        $questions = sfsMemberSecretQuestionsPeer::getAllQuestionsWithI18n();
+
+        if ($questions !== null) {
+            $arrayQuestions[] = '';
+            foreach ($questions as $question) {
+                $arrayQuestions[$question->getQuestion()] = $question->getQuestion();
+            }
+        }
+
+        $this->setWidgets(
+            array(
+                'email'              => new sfWidgetFormInput(),
+                'first_name'         => new sfWidgetFormInput(),
+                'last_name'          => new sfWidgetFormInput(),
+                'phone'              => new sfWidgetFormInput(),
+                'mobile_phone'       => new sfWidgetFormInput(),
+                'password'           => new sfWidgetFormInputPassword(),
+                'confirm_password'   => new sfWidgetFormInputPassword(),
+                'secret_question'    => new sfWidgetFormSelect(array('choices' => $arrayQuestions)),
+                'secret_answer'      => new sfWidgetFormInput()
+             )
+        );
+        
+        $validatorEmail = new sfValidatorAnd(
+            array(
+                new sfValidatorEmail(
+                    array('required' => true), 
+                    array('invalid' => __('This is not a valid email address'))
+                ),
+                new sfsValidatorMember(
+                    array('check_email' => true),
+                    array('check_email' => 'dasas')
+                )
+            )
+        );
+        
+        $validatorPassword = new sfValidatorString(
+            array(
+                'required' => true, 
+                'min_length' => 6 , 
+                'max_length' => 20
+            ),
+            array('invalid' => __('assa'))
+        );
+        
+        $validatorConfirmPassword = new sfValidatorPass(
+            array('required' => true)
+        );
+        
+        $validatorFirstName = new sfValidatorString(
+            array('required' => true, 'min_length' => 4)
+        );
+        
+        $validatorLastName = new sfValidatorString(
+            array('required' => true, 'min_length' => 4)
+        );
+        
+        $validatorSecretQuestion = new sfValidatorChoice(
+            array('choices' => $arrayQuestions)
+        );
+        
+        $validatorSecretAnswer = new sfValidatorString(
+            array('required' => true, 'min_length' => 4)
+        );
+        
+        $this->setValidators(
+            array(
+               'email'            => $validatorEmail,
+               'password'         => $validatorPassword,
+               'confirm_password' => $validatorConfirmPassword,
+               'first_name'       => $validatorFirstName,
+               'last_name'        => $validatorLastName,
+               'secret_question'  => $validatorSecretQuestion,
+               'secret_answer'    => $validatorSecretAnswer
+            )
+        );
+
+        $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+    }
+}
