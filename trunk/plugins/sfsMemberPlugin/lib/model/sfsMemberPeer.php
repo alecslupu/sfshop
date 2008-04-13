9c1b77ca966c96ffd01890503b453c0c87a9579b
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-04-13 14:32:50 +0000

    added function retrieveByConfirmCode

--- trunk/plugins/sfsMemberPlugin/lib/model/sfsMemberPeer.php
+++ trunk/plugins/sfsMemberPlugin/lib/model/sfsMemberPeer.php
@@ -24,8 +24,28 @@ class sfsMemberPeer extends BasesfsMemberPeer
     public static function retrieveByEmail($email)
     {
         $criteria = new Criteria();
-        $criteria->add(sfsMemberPeer::EMAIL, $email);
-        return sfsMemberPeer::doSelectOne($criteria);
+        $criteria->add(self::EMAIL, $email);
+        return self::doSelectOne($criteria);
+    }
+    
+    /**
+    * Get member object by confirm code.
+    *
+    * @param  string $email
+    * @return mixed if member exist returns object, otherwise null.
+    * @author Dmitry Nesteruk
+    * @access public
+    */
+    public static function retrieveByConfirmCode($confirmCode)
+    {
+        if (self::$member == null) {
+            $criteria = new Criteria();
+            $criteria->add(self::CONFIRM_CODE, $confirmCode);
+            return self::doSelectOne($criteria);
+        }
+        else {
+            return self::$member;
+        }
     }
     
     /**
