a62af03f412ef17eb1ae3bdb3b7a267a6d9763cb
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-04-17 15:28:12 +0000

    added function for save address book

--- trunk/plugins/sfsMemberPlugin/lib/model/sfsAddressBookPeer.php
+++ trunk/plugins/sfsMemberPlugin/lib/model/sfsAddressBookPeer.php
@@ -9,4 +9,20 @@
  */ 
 class sfsAddressBookPeer extends BasesfsAddressBookPeer
 {
+    
+    /**
+    * Saves data entered by user.
+    *
+    * @param  array $field associate array with values of address book.
+    * @return object
+    * @author Dmitry Nesteruk
+    * @access public
+    */
+    public static function saveAddressBook(array $fields)
+    {
+        $addressBook = new sfsAddressBook();
+        $addressBook->fromArray($fields);
+        $addressBook->save();
+        return $addressBook;
+    }
 }
