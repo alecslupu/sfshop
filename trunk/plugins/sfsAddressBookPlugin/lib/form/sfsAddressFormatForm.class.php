4455f5e6087677ab6efcba070a39e4a1fcf1c308
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-04-21 19:35:18 +0000

    added table for address format

---
+++ trunk/plugins/sfsAddressBookPlugin/lib/form/sfsAddressFormatForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * sfsAddressFormat form.
+ *
+ * @package    form
+ * @subpackage address_format
+ * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class sfsAddressFormatForm extends BasesfsAddressFormatForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ trunk/plugins/sfsAddressBookPlugin/lib/model/sfsAddressFormatPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'address_format' table.
+ *
+ * 
+ *
+ * @package plugins.sfsAddressBookPlugin.lib.model
+ */ 
+class sfsAddressFormatPeer extends BasesfsAddressFormatPeer
+{
+}
