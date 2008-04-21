93dee33f09843893b43ca46a362e6b009b104d6a
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-04-21 15:49:40 +0000

    added formatter list for widget schema

---
+++ trunk/plugins/sfsCorePlugin/lib/widget/sfsWidgetFormSchemaFormatterList.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ *
+ * @package    symfony
+ * @subpackage plugin.sfsCorePlugin.widget
+ * @author     Dmitry Nesteruk
+ * @version    SVN: $Id: sfsWidgetFormSchemaFormatterList.class.php 5995 2007-11-13 15:50:03Z fabien $
+ */
+class sfsWidgetFormSchemaFormatterList extends sfWidgetFormSchemaFormatter
+{
+  protected
+    $rowFormat       = "<li>\n %label%\n %error%\n %field%%help%\n%hidden_fields%</li>\n",
+    $errorRowFormat  = "<li>\n%errors%</li>\n",
+    $helpFormat      = '<br />%help%',
+    $decoratorFormat = "<ul>\n %content%</ul>";
+}
