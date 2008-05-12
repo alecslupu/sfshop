10570d483debadd01132f85c8ab3dfc102bc70db
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-05-12 15:22:49 +0000

    added link to add new address

--- trunk/plugins/sfsAddressBookPlugin/modules/addressBook/templates/myAddressesListSuccess.php
+++ trunk/plugins/sfsAddressBookPlugin/modules/addressBook/templates/myAddressesListSuccess.php
@@ -1,5 +1,7 @@
 <h3><?php echo __('My address book') ?></h3>
 
 <div>
-<?php include_partial('my_addresses_list', array('pager' => $pager)) ?>
+    <?php include_partial('my_addresses_list', array('pager' => $pager)) ?>
 </div>
+<div style="float: right"><?php echo link_to(__('Add address'), '@addressBook_addAddress') ?></div>
+<br/>
