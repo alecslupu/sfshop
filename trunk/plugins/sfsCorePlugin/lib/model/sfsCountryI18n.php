858e94e5909f10176e638fc8d734870cd44f3b1f
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-05-21 12:01:39 +0000

    chaged schema for languages and countries table, added core module with base structure

---
+++ trunk/plugins/sfsCorePlugin/lib/model/sfsCountryI18n.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'countries_i18n' table.
+ *
+ * 
+ *
+ * @package plugins.sfsCorePlugin.lib.model
+ */ 
+class sfsCountryI18n extends BasesfsCountryI18n
+{
+}
---
+++ trunk/plugins/sfsCorePlugin/lib/model/sfsCountryI18nPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'countries_i18n' table.
+ *
+ * 
+ *
+ * @package plugins.sfsCorePlugin.lib.model
+ */ 
+class sfsCountryI18nPeer extends BasesfsCountryI18nPeer
+{
+}
