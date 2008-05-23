372f94de2c29d91fe1a382c4258f936cdd17131e
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-05-23 07:56:21 +0000

    generated categories module

--- trunk/plugins/sfsCategoryPlugin/lib/model/sfsCategory.php
+++ trunk/plugins/sfsCategoryPlugin/lib/model/sfsCategory.php
@@ -9,5 +9,82 @@
  */ 
 class sfsCategory extends BasesfsCategory
 {
-
+    protected $parentTreeArray = array();
+    
+    /**
+     * Gets child categories for current category.
+     *
+     * @param  void
+     * @return array
+     * @author Dmitry Nesteruk
+     * @access public
+     */
+    public function getChildCategories($culture = null)
+    {
+        if ($culture == null) {
+            $culture = sfContext::getInstance()->getUser()->getCulture();
+        }
+        
+        if ($this->getHasChild()) {
+            $criteria = new Criteria();
+            $criteria->add(sfsCategoryPeer::IS_ACTIVE, sfsCategoryPeer::ACTIVE);
+            $criteria->add(sfsCategoryPeer::PARENT_ID, $this->getId());
+            $criteria->addAscendingOrderByColumn(sfsCategoryPeer::TITLE);
+            return sfsCategoryPeer::doSelectWithI18n($criteria, $culture);
+        }
+        else {
+            return null;
+        }
+    }
+    
+    public function getAllChildCategories(&$allChildCategories)
+    {
+        $categories = $this->getChildCategories();
+        
+        if (is_array($categories) && !empty($categories)) {
+            foreach ($categories as $category) {
+                $allChildCategories[] = $category;
+                $category->getAllChildCategories($allChildCategories);
+            }
+        }
+    }
+    
+    /**
+     * Returns array with parents series for current category.
+     *
+     * @param  void
+     * @return array
+     * @author Dmitry Nesteruk
+     * @access public
+     */
+    public function getParentsSeries(&$array)
+    {
+        $id = $this->getParentId();
+        
+        if ($id == null) {
+            $array = array_reverse($array);
+            return $array;
+        }
+        
+        $category = sfsCategoryPeer::retrieveByPK($id);
+        
+        if (is_object($category)) {
+            $array[] = $category;
+            $category->getParentsSeries($array);
+        }
+    }
+    
+    public function getLastParentCategoryId()
+    {
+        
+        if (!empty($this->parentTreeArray)) {
+            return $this->parentTreeArray[0]->getId();
+        }
+        
+        $this->getParentTree();
+        
+        if (!empty($this->parentTreeArray)) {
+            return $this->parentTreeArray[0]->getId();
+        }
+    }
 }
--- trunk/plugins/sfsCategoryPlugin/lib/model/sfsCategoryPeer.php
+++ trunk/plugins/sfsCategoryPlugin/lib/model/sfsCategoryPeer.php
@@ -9,4 +9,20 @@
  */ 
 class sfsCategoryPeer extends BasesfsCategoryPeer
 {
+    /**
+    * Gets all avaliable categories.
+    *
+    * @param  void
+    * @return array with objects, otherwise null.
+    * @author Dmitry Nesteruk
+    * @access public
+    */
+    public static function getAll()
+    {
+        $criteria = new Criteria();
+        $criteria->add(self::IS_ACTIVE, 1);
+        $criteria->add(self::PARENT_ID, 0);
+        $criteria->addAscendingOrderByColumn(self::TITLE);
+        return self::doSelectWithI18n($criteria);
+    }
 }
