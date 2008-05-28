7f4f46e2c419d7a35cd9ac5d20d87f6dc1ddedf8
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-05-28 08:49:06 +0000

    added orm classses for table products2categories

---
+++ trunk/plugins/sfsProductPlugin/lib/model/sfsProducts2Categories.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'products2categories' table.
+ *
+ * 
+ *
+ * @package plugins.sfsProductPlugin.lib.model
+ */ 
+class sfsProducts2Categories extends BasesfsProducts2Categories
+{
+}
---
+++ trunk/plugins/sfsProductPlugin/lib/model/sfsProducts2CategoriesPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'products2categories' table.
+ *
+ * 
+ *
+ * @package plugins.sfsProductPlugin.lib.model
+ */ 
+class sfsProducts2CategoriesPeer extends BasesfsProducts2CategoriesPeer
+{
+}
