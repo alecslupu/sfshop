b5af4a1c7f354bcf169b3f79d4582bb87487a087
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-05-28 08:49:38 +0000

    added form classs for table products2categories

---
+++ trunk/plugins/sfsProductPlugin/lib/form/sfsProducts2CategoriesForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * sfsProducts2Categories form.
+ *
+ * @package    form
+ * @subpackage products2categories
+ * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class sfsProducts2CategoriesForm extends BasesfsProducts2CategoriesForm
+{
+  public function configure()
+  {
+  }
+}
