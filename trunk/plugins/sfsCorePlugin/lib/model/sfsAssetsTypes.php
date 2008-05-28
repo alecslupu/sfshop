de528cdf17e10f753c3ccb6853673a63c16e2a03
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-05-28 10:18:16 +0000

    added orm classes for assets_types table

---
+++ trunk/plugins/sfsCorePlugin/lib/model/sfsAssetsTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'assets_types' table.
+ *
+ * 
+ *
+ * @package plugins.sfsCorePlugin.lib.model
+ */ 
+class sfsAssetsTypes extends BasesfsAssetsTypes
+{
+}
---
+++ trunk/plugins/sfsCorePlugin/lib/model/sfsAssetsTypesPeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'assets_types' table.
+ *
+ * 
+ *
+ * @package plugins.sfsCorePlugin.lib.model
+ */ 
+class sfsAssetsTypesPeer extends BasesfsAssetsTypesPeer
+{
+}
