2b0d2b45a9f2b1e7e1717b2aa4dfeb41fd32e354
nesterukD <nesterukD@e1a02840-6048-0410-8644-cfba31147e63>
2008-08-13 17:25:28 +0000

    added data and doc directory

---
+++ trunk/data/sql/plugins.sfsOrderPlugin.lib.model.schema.sql
@@ -0,0 +1,193 @@
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+#-----------------------------------------------------------------------------
+#-- order_item
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_item`;
+
+
+CREATE TABLE `order_item`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`uuid` VARCHAR(32)  NOT NULL,
+	`member_id` INTEGER  NOT NULL,
+	`member_first_name` VARCHAR(128),
+	`member_last_name` VARCHAR(128),
+	`member_country_id` INTEGER,
+	`member_state_id` INTEGER,
+	`member_state_title` VARCHAR(64),
+	`member_city` VARCHAR(64),
+	`member_street` VARCHAR(128),
+	`member_postcode` VARCHAR(16),
+	`billing_first_name` VARCHAR(128),
+	`billing_last_name` VARCHAR(128),
+	`billing_country_id` INTEGER,
+	`billing_state_id` INTEGER,
+	`billing_state_title` VARCHAR(64),
+	`billing_city` VARCHAR(64),
+	`billing_street` VARCHAR(128),
+	`billing_postcode` VARCHAR(16),
+	`delivery_first_name` VARCHAR(128),
+	`delivery_last_name` VARCHAR(128),
+	`delivery_country_id` INTEGER,
+	`delivery_state_id` INTEGER,
+	`delivery_state_title` VARCHAR(64),
+	`delivery_city` VARCHAR(64),
+	`delivery_street` VARCHAR(128),
+	`delivery_postcode` VARCHAR(16),
+	`delivery_cost` DECIMAL(10,2) default 0.00,
+	`delivery_description` TEXT,
+	`status_id` INTEGER,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `order_item_U_1` (`uuid`),
+	INDEX `order_item_FI_1` (`member_id`),
+	CONSTRAINT `order_item_FK_1`
+		FOREIGN KEY (`member_id`)
+		REFERENCES `member` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_2` (`member_country_id`),
+	CONSTRAINT `order_item_FK_2`
+		FOREIGN KEY (`member_country_id`)
+		REFERENCES `country` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_3` (`member_state_id`),
+	CONSTRAINT `order_item_FK_3`
+		FOREIGN KEY (`member_state_id`)
+		REFERENCES `state` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_4` (`billing_country_id`),
+	CONSTRAINT `order_item_FK_4`
+		FOREIGN KEY (`billing_country_id`)
+		REFERENCES `country` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_5` (`billing_state_id`),
+	CONSTRAINT `order_item_FK_5`
+		FOREIGN KEY (`billing_state_id`)
+		REFERENCES `state` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_6` (`delivery_country_id`),
+	CONSTRAINT `order_item_FK_6`
+		FOREIGN KEY (`delivery_country_id`)
+		REFERENCES `country` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_7` (`delivery_state_id`),
+	CONSTRAINT `order_item_FK_7`
+		FOREIGN KEY (`delivery_state_id`)
+		REFERENCES `state` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE,
+	INDEX `order_item_FI_8` (`status_id`),
+	CONSTRAINT `order_item_FK_8`
+		FOREIGN KEY (`status_id`)
+		REFERENCES `order_status` (`id`)
+		ON UPDATE RESTRICT
+		ON DELETE CASCADE
+)Type=InnoDB;
+
+#-----------------------------------------------------------------------------
+#-- order_status
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_status`;
+
+
+CREATE TABLE `order_status`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(128),
+	`is_active` INTEGER default 0 NOT NULL,
+	`created_at` DATETIME,
+	`updated_at` DATETIME,
+	PRIMARY KEY (`id`)
+)Type=InnoDB;
+
+#-----------------------------------------------------------------------------
+#-- order_status_i18n
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_status_i18n`;
+
+
+CREATE TABLE `order_status_i18n`
+(
+	`id` INTEGER  NOT NULL,
+	`culture` VARCHAR(7)  NOT NULL,
+	`title` VARCHAR(128),
+	PRIMARY KEY (`id`,`culture`),
+	CONSTRAINT `order_status_i18n_FK_1`
+		FOREIGN KEY (`id`)
+		REFERENCES `order_status` (`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+)Type=InnoDB;
+
+#-----------------------------------------------------------------------------
+#-- order_product
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_product`;
+
+
+CREATE TABLE `order_product`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`order_item_id` INTEGER  NOT NULL,
+	`product_id` INTEGER  NOT NULL,
+	`price` DECIMAL(10,2),
+	`quantity` INTEGER default 1 NOT NULL,
+	`created_at` DATETIME,
+	PRIMARY KEY (`id`),
+	INDEX `order_product_FI_1` (`order_item_id`),
+	CONSTRAINT `order_product_FK_1`
+		FOREIGN KEY (`order_item_id`)
+		REFERENCES `order_item` (`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	INDEX `order_product_FI_2` (`product_id`),
+	CONSTRAINT `order_product_FK_2`
+		FOREIGN KEY (`product_id`)
+		REFERENCES `product` (`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+)Type=InnoDB;
+
+#-----------------------------------------------------------------------------
+#-- order_product2option_product
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_product2option_product`;
+
+
+CREATE TABLE `order_product2option_product`
+(
+	`order_product_id` INTEGER  NOT NULL,
+	`option_product_id` INTEGER  NOT NULL,
+	`created_at` DATETIME,
+	PRIMARY KEY (`order_product_id`,`option_product_id`),
+	CONSTRAINT `order_product2option_product_FK_1`
+		FOREIGN KEY (`order_product_id`)
+		REFERENCES `order_product` (`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	INDEX `order_product2option_product_FI_2` (`option_product_id`),
+	CONSTRAINT `order_product2option_product_FK_2`
+		FOREIGN KEY (`option_product_id`)
+		REFERENCES `option_product` (`id`)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+)Type=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
