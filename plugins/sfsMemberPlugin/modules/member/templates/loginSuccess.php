464aba4c28fd2ef96a6a61ea85ce334c05c7f765
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-10-01 08:01:49 +0000

    refactored member relate functionality

--- plugins/sfsMemberPlugin/modules/member/templates/loginSuccess.php
+++ plugins/sfsMemberPlugin/modules/member/templates/loginSuccess.php
@@ -1,8 +1,12 @@
 <?php include_partial('core/container_header', array('caption' => __('Sign in'))) ?>
-    <form action="<?php echo url_for('@member_login'); ?>" method="post" class="form">
+    <form action="<?php echo url_for('@member_login'); ?>" method="post" class="form" id="form_login">
         <ul class="main">
             <?php echo $form ?>
             <li><input type="submit" value="<?php echo __('Login') ?>" class="button"></li>
         </ul>
     </form>
 <?php include_partial('core/container_footer') ?>
+
+<?php echo javascript_tag('
+    highlightFieldsWithError("form_login");
+') ?>
