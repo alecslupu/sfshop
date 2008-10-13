8fafcdae3b9d6fd2b7a7526b94a901e200b5f03c
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-10-13 08:56:43 +0000

    renamed sfsOrderSelectAddressBookForm.class.php on sfsAddressBookSelectForm.class.phpand sfsOrderInputAddressBookForm.class.php on sfsAddressBookInputForm.class.php

---
+++ plugins/sfsAddressBookPlugin/lib/form/sfsAddressBookInputForm.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
+ * AddressBookInput form.
+ *
+ * @package    plugin.sfsAddressBookPlugin
+ * @subpackage lib.form
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class sfsAddressBookInputForm extends AddressBookForm
+{
+    public function configure()
+    {
+        parent::configure();
+        $this->offsetUnset('is_default');
+    }
+}
---
+++ plugins/sfsAddressBookPlugin/lib/form/sfsAddressBookSelectForm.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
+ * sfsAddressBookSelectForm form.
+ *
+ * @package    plugin.sfsAddressBookPlugin
+ * @subpackage lib.form
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: sfsAddressBookSelectForm.class.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class sfsAddressBookSelectForm extends AddressBookForm
+{
+    public function configure()
+    {
+        parent::configure();
+        
+        $arrayAddresses = AddressBookPeer::getHashByMemberId(sfContext::getInstance()->getUser()->getUserId());
+        
+        if (count($arrayAddresses) > 0) {
+            $this->setWidgets(
+                array('address_id' => new sfWidgetFormSelect(array('choices' => $arrayAddresses)))
+            );
+            
+            $this->widgetSchema->setLabel('address_id', ' ');
+            
+            $validatorAddressId = new sfValidatorChoice(
+                array('choices' => array_keys($arrayAddresses))
+            );
+            
+            $this->setValidators(array('address_id' => $validatorAddressId));
+        }
+        $this->getWidgetSchema()->setNameFormat('address[%s]');
+    }
+}
