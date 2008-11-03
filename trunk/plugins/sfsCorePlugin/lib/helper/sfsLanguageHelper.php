6d8be264f0b0480460621f2f41dc526fcd1eeb29
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-03 12:22:32 +0000

    added russian language, added russian translation for products and categories

--- trunk/plugins/sfsCorePlugin/lib/helper/sfsLanguageHelper.php
+++ trunk/plugins/sfsCorePlugin/lib/helper/sfsLanguageHelper.php
@@ -1,15 +1,25 @@
 <?php
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
 function language_icon($language)
 { 
     if ($language !== null) {
         return image_tag(
             sfConfig::get('languages_images_dir', 'languages') 
             . '/'
-            . strtolower($language->getNameEnglish()) 
+            . strtolower($language->getTitleEnglish()) 
             . '/'
             . 'icon.png',
             array(
-                'title' => $language->getNameOwn(),
+                'title' => $language->getTitleOwn(),
+                'alt'   => $language->getTitleOwn(),
                 'align' => 'absmiddle'
             )
         );
