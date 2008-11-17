3ae04b37847b9158321929dddc4e3b7438e6a1e6
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-17 11:53:06 +0000

    renamed exists actions/components class to base actions/components, created new empty actions/components class (Task #29)

--- trunk/plugins/sfsDeliveryPlugin/modules/delivery/actions/components.class.php
+++ trunk/plugins/sfsDeliveryPlugin/modules/delivery/actions/components.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * sfShop, open source e-commerce solutions.
  * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
@@ -8,63 +9,17 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require_once(dirname(__FILE__).'/../lib/BaseDeliveryComponents.class.php');
+
 /**
  * Delivery components.
  *
- * @package    sfShop
- * @subpackage delivery
+ * @package    plugins.sfsDeliveryPlugin
+ * @subpackage modules.delivery
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: components.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
-class deliveryComponents extends sfComponents
+class deliveryComponents extends BaseDeliveryComponents
 {
-   /**
-    * Delivery method info.
-    *
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeOrderDeliveryInfo()
-    {
-        $sfUser = $this->getUser();
-        
-        $methodId = $sfUser->getAttribute('method_id', null, 'order/delivery');
-        
-        if ($methodId != null) {
-            list($methodId, $subMethodId) = $methodId;
-            
-            $criteria = new Criteria();
-            $this->deliveryService = DeliveryPeer::retrieveById($methodId, $criteria, true);
-            
-            if ($this->deliveryService == null) {
-                sfContext::getInstance()->getController()->redirect('@delivery_checkout');
-            }
-            
-            $this->methodTitle =  $sfUser->getAttribute('method_title', null, 'order/delivery');
-            $this->methodPrice = $sfUser->getAttribute('price', null, 'order/delivery');
-        }
-        else {
-            sfContext::getInstance()->getController()->redirect('@delivery_checkout');
-        }
-    }
-    
-   /**
-    * Form for select some delivery service.
-    *
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeSelectForm()
-    {
-        $this->form = new sfsDeliverySelectForm();
-        
-        $defaultMethodId = $this->getUser()->getAttribute('method_id', null, 'order/delivery');
-        $this->form->setDefault('method_id', $defaultMethodId);
-        
-        $this->errors = $this->form->getServiceErrors();
-        $this->sections = $this->form->getSections();
-    }
+
 }
