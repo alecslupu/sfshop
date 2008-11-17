bc2b87329e4b8810ad58a92f66c2c94ba7597040
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-17 11:14:59 +0000

    renamed exists actions/components class to base actions/components, created new empty actions/components class (Task #29)

--- trunk/plugins/sfsCategoryPlugin/modules/category/actions/components.class.php
+++ trunk/plugins/sfsCategoryPlugin/modules/category/actions/components.class.php
@@ -9,6 +9,8 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require_once(dirname(__FILE__).'/../lib/BaseCategoryActions.class.php');
+
 /**
  * Categories components.
  *
@@ -17,72 +19,7 @@
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: components.class.php 2692 2006-11-15 21:03:55Z fabien $
  */
-class categoryComponents extends sfComponents 
+class categoryComponents extends BaseCategoryComponents
 {
-   /**
-    * Draws categories tree.
-    *
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeMenuTree()
-    {
-        sfLoader::loadHelpers('sfsCategory');
-        
-        $request = $this->getRequest();
-        $this->categories = CategoryPeer::getFirstLevel();
-        $this->currentCategoryId = 0;
-        
-        $this->parentTree = array();
-        
-        if ($request->hasParameter('path')) {
-            $this->currentCategoryId = get_current_category_id();
-            
-            $currentCategory = CategoryPeer::retrieveByPK($this->currentCategoryId);
-            
-            $controller = $this->getController();
-            $actionInstance = $controller->getActionStack()->getLastEntry()->getActionInstance();
-            $actionInstance->forward404Unless($currentCategory);
-            
-            $response = $this->getResponse();
-            $response->addMeta('keywords', $currentCategory->getMetaKeywords(), true);
-            $response->addMeta('description', $currentCategory->getMetaDescription(), true);
-            
-            if ($currentCategory !== null) {
-                $currentCategory->getParentsSeries($this->parentTree);
-                $this->parentTree = array_merge($this->parentTree, array($currentCategory));
-            }
-        }
-        
-        if ($request->hasParameter('is_search')) {
-            $this->itemRouting = '@product_search';
-        }
-        else {
-            $this->itemRouting = '@product_list';
-        }
-    }
-    
-   /**
-    * Draws header for product list.
-    * 
-    * @param  $criteria
-    * @return void
-    * @author Dmitry Nesteruk
-    * @access public
-    */
-    public function executeHeaderProductList()
-    {
-        sfLoader::loadHelpers('sfsCategory');
-        $categoryId = get_current_category_id();
-        
-        $this->category = CategoryPeer::retrieveByPK($categoryId);
-        
-        if ($this->category !== null) {
-            $response = $this->getResponse();
-            $response->addMeta('keywords', $this->category->getMetaKeywords(), true);
-            $response->addMeta('description', $this->category->getMetaDescription(), true);
-        }
-    }
+
 }
--- trunk/plugins/sfsCategoryPlugin/modules/category/lib/BaseCategoryActions.class.php
+++ trunk/plugins/sfsCategoryPlugin/modules/category/lib/BaseCategoryActions.class.php
@@ -9,16 +9,14 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
-require_once(dirname(__FILE__).'/../lib/BaseCategoryActions.class.php');
-
 /**
- * category actions.
+ * Base category actions.
  *
  * @package    plugins.sfsCategoryPlugin
  * @subpackage modules.category
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
  */
-class categoryActions extends BaseCategoryActions
+class BaseCategoryActions extends sfActions
 {
 }
