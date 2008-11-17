4d48b168854e9b8da316acda9197b42479c9529e
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-17 11:07:17 +0000

    renamed exists actions/components class to base actions/components, created new empty actions/components class (Task #29)

--- trunk/plugins/sfsAddressBookPlugin/modules/addressBook/actions/components.class.php
+++ trunk/plugins/sfsAddressBookPlugin/modules/addressBook/actions/components.class.php
@@ -8,6 +8,8 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require_once(dirname(__FILE__).'/../lib/BaseAddressBookComponents.class.php');
+
 /**
  * AddressBook components.
  *
@@ -15,99 +17,7 @@
  * @subpackage modules.addressBook
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  */
-class addressBookComponents extends sfComponents
+class addressBookComponents extends BaseAddressBookComponents
 {
-   /**
-    * Form for input address.
-    *
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeInputForm()
-    {
-        $response = $this->getResponse();
-        $response->addJavaScript('/js/sfsForm.js');
-        
-        if (isset($this->address)) {
-            $address = $this->address;
-            $isEditAddress = true;
-        }
-        else {
-            $address = new AddressBook();
-            
-            $member = $this->getUser()->getUser();
-            
-            if ($member !== null) {
-                $address->setFirstName($member->getFirstName());
-                $address->setLastName($member->getLastName());
-            }
-        }
-        
-        $this->form = new sfsAddressBookInputForm($address);
-        
-        if (isset($isEditAddress)) {
-            $this->form->getWidgetSchema()->offsetSet('id', new sfWidgetFormInputHidden());
-            $this->form->setDefault('id', $address->getId());
-        }
-    }
-    
-   /**
-    * Form for select exists address from address book.
-    *
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeSelectForm()
-    {
-        $response = $this->getResponse();
-        $response->addJavaScript('/js/sfsForm.js');
-        
-        $this->form = new sfsAddressBookSelectForm();
-        
-        $default = AddressBookPeer::retrieveDefaultByMemberId($this->getUser()->getUserId());
-        
-        if ($default !== null) {
-            $defaultAddressId = $default->getId();
-            $this->form->setDefault('address_id', $default->getId());
-        }
-        
-        $this->hasAddresses = AddressBookPeer::hasAddresses($this->getUser()->getUserId());
-    }
-    
-    public function executeSelectAddress()
-    {
-        $this->hasAddresses = AddressBookPeer::hasAddresses($this->getUser()->getUserId());
-    }
-    
-   /**
-    * Gets details delivery address.
-    * 
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeDeliveryAddress()
-    {
-        $addressId = $this->getUser()->getAttribute('address_id', null, 'order/delivery');
-        $this->address = AddressBookPeer::retrieveById($addressId);
-    }
-    
-   /**
-    * Gets details billing address.
-    * 
-    * @param  void
-    * @return void
-    * @author Dmitry Nesteruk <nest@dev-zp.com>
-    * @access public
-    */
-    public function executeBillingAddress()
-    {
-        $addressId = $this->getUser()->getAttribute('address_id', null, 'order/billing');
-        $this->address = AddressBookPeer::retrieveById($addressId);
-    }
+
 }
