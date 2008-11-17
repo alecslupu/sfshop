4ea08edbbd31e47d0159445ca1cfc648a95b3ca7
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-17 20:49:22 +0000

    enabled information module

--- trunk/plugins/sfsAddressBookPlugin/modules/addressBook/actions/actions.class.php
+++ trunk/plugins/sfsAddressBookPlugin/modules/addressBook/actions/actions.class.php
@@ -16,7 +16,7 @@ require_once(dirname(__FILE__).'/../lib/BaseAddressBookActions.class.php');
  *
  * @package    plugin.sfsAddressBookPlugin
  * @subpackage modules.addressBook
- * @author     Dmitry Nesteruk  <nest@dev-zp.com>
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
  */
 class addressBookActions extends BaseAddressBookActions
--- trunk/plugins/sfsProductPlugin/modules/product/actions/components.class.php
+++ trunk/plugins/sfsProductPlugin/modules/product/actions/components.class.php
@@ -1,11 +1,20 @@
 <?php
 
 /**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
  * Product actions.
  *
- * @package    plugins.sfsProductsPlugin
+ * @package    plugins.sfsProductPlugin
  * @subpackage modules.product
- * @author     Dmitry Nesteruk
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
 class productComponents extends sfComponents
