b57b7bb6b245aa87b8a4a855faeb0c1b13a75c32
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-18 09:36:10 +0000

    renamed exists actions/components class to base actions/components, created new empty actions/components class (Task #29)

--- plugins/sfsPaymentPaypalPlugin/modules/paypal/actions/actions.class.php
+++ plugins/sfsPaymentPaypalPlugin/modules/paypal/actions/actions.class.php
@@ -9,6 +9,8 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require_once(dirname(__FILE__).'/../lib/BasePaypalActions.class.php');
+
 /**
  * Paypal actions.
  *
@@ -17,66 +19,7 @@
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
-class paypalActions extends sfsPaymentActions
+class paypalActions extends BasePaypalActions
 {
-    public function executeIndex($request)
-    {
-        $sfUser = $this->getUser();
-        
-        $orderItem = $this->getOrderItemObjectByIdOrUuid($this->getRequestParameter('order_item_id'));
-        $paymentService = $this->getPaymentServiceObject();
-        $params = sfsJSONPeer::decode($paymentService->getParams());
-        
-        $cc = new sfPaypalDirect(sfConfig::get('sf_root_dir') . DIRECTORY_SEPARATOR . 'paypal/php-sdk/lib');
-        $cc->setUserName($params['username']);
-        $cc->setPassword($params['password']);
-        $cc->setSignature($params['signature']);
-        $cc->setTestMode($params['test_mode']);
-        
-        $total = $orderItem->getTotalPrice();
-        
-        $cc->setTransactionTotal(format_currency($total, 'USD', true));
-        $cc->setTransactionDescription('Registration');
-        
-        $cc->setCancelURL(url_for('@order_paymentSuspend', true));
-        $cc->setReturnURL(url_for('@payment_paypalCharge?uuid=' . $orderItem->getUuid(), true));
-        
-        $goto = $cc->GetExpressUrl();
-        
-        if ($goto) {
-            $this->redirect($goto);
-        }
-        else {
-            $this->logMessage('Payment (Paypal service): ' . $cc->getErrorString(), sfLogger::CRIT);
-            $this->redirect('@payment_chargeFailed');
-        }
-    }
-    
-    public function executeCharge($request)
-    {
-        $paymentService = $this->getPaymentServiceObject('paypal');
-        $orderItem = $this->getOrderItemObjectByIdOrUuid($request->getParameter('uuid'));
-        
-        $params = sfsJSONPeer::decode($paymentService->getParams());
-        
-        $cc = new sfPaypalDirect(sfConfig::get('sf_root_dir') . DIRECTORY_SEPARATOR . 'paypal/php-sdk/lib');
-        
-        $cc->setUserName($params['username']);
-        $cc->setPassword($params['password']);
-        $cc->setSignature($params['signature']);
-        $cc->setTestMode($params['test_mode']);
-        
-        $total = $orderItem->getTotalPrice();
-        
-        $cc->setTransactionTotal(format_currency($total, 'USD', true));
-        
-        if ($cc->chargeExpressCheckout($request->getParameter('token'))) {
-            $this->setOrderStatusProcessing();
-            $this->redirect('@order_checkoutFinished');
-        }
-        else {
-            $this->logMessage('Payment (Paypal service): ' . $cc->getErrorString(), sfLogger::CRIT);
-            $this->redirect('@payment_chargeFailed');
-        }
-    }
+
 }
