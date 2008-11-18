379dfabee4f9b466f13daf170e7fa4ecd972569f
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-18 09:32:28 +0000

    renamed exists actions/components class to base actions/components, created new empty actions/components class (Task #29)

--- plugins/sfsPaymentAuthorizeNetPlugin/modules/authorizeNet/lib/BaseAuthorizeNetActions.class.php
+++ plugins/sfsPaymentAuthorizeNetPlugin/modules/authorizeNet/lib/BaseAuthorizeNetActions.class.php
@@ -9,17 +9,15 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
-require_once(dirname(__FILE__).'/../lib/BaseAuthorizeNetActions.class.php');
-
 /**
- * AuthorizeNet actions.
+ * Base authorizeNet actions.
  *
  * @package    plugins.sfsPaymentAuthorizeNetPlugin
  * @subpackage modules.authorizeNet
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
-class authorizeNetActions extends BaseAuthorizeNetActions
+class BaseAuthorizeNetActions extends sfsPaymentActions
 {
    /**
     * Gets card info.
--- plugins/sfsPaymentAuthorizeNetPlugin/modules/authorizeNet/lib/BaseAuthorizeNetComponents.class.php
+++ plugins/sfsPaymentAuthorizeNetPlugin/modules/authorizeNet/lib/BaseAuthorizeNetComponents.class.php
@@ -9,17 +9,15 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
-require_once(dirname(__FILE__).'/../lib/BaseAuthorizeNetComponents.class.php');
-
 /**
- * AuthorizeNet components.
+ * Base authorizeNet components.
  *
  * @package    plugins.sfsPaymentAuthorizeNetPlugin
  * @subpackage modules.authorizeNet
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
-class authorizeNetComponents extends BaseAuthorizeNetComponents
+class BaseAuthorizeNetComponents extends sfComponents
 {
    /**
     * Form for set credit card info.
