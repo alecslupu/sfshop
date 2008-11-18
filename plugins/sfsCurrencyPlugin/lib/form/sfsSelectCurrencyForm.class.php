94339f45ae9d7213e1b87c505febccb6233434d5
g21.michal <g21.michal@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-18 19:12:38 +0000

    hide select currency box where there's only one value (Issue 30)

--- plugins/sfsCurrencyPlugin/lib/form/sfsSelectCurrencyForm.class.php
+++ plugins/sfsCurrencyPlugin/lib/form/sfsSelectCurrencyForm.class.php
@@ -9,26 +9,38 @@
  */
 class sfsSelectCurrencyForm extends BaseCurrencyForm
 {
+    private $arrayCurrencies;
+
     public function configure()
     {
         parent::configure();
         
         $criteria = new Criteria();
         CurrencyPeer::addPublicCriteria($criteria);
-        $arrayCurrencies = CurrencyPeer::getHash($criteria);
+        $this->arrayCurrencies = CurrencyPeer::getHash($criteria);
         
         $this->setWidgets(
-            array('id' => new sfWidgetFormSelect(array('choices' => $arrayCurrencies), array('onchange' => 'this.form.submit()')))
+            array('id' => new sfWidgetFormSelect(array('choices' => $this->arrayCurrencies), array('onchange' => 'this.form.submit()')))
         );
 
         $this->widgetSchema->setLabel('id', 'Currency');
         
         $validatorCurrencyId = new sfValidatorChoice(
-            array('choices' => array_keys($arrayCurrencies))
+            array('choices' => array_keys($this->arrayCurrencies))
         );
         
         $this->setValidators(array('id' => $validatorCurrencyId));
         $this->getWidgetSchema()->setNameFormat('currency[%s]');
         $this->defineSfsListFormatter();
+    }
+
+    /**
+     * Gets array of currencies
+     * 
+     * @return array
+     */
+    public function getArrayCurrencies()
+    {
+        return $this->arrayCurrencies;
     }
 }
