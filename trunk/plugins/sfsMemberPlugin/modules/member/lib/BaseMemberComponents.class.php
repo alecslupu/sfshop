8feeb20dce302b52e1a157f541ac1bc73dd16929
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-18 09:26:47 +0000

    renamed exists actions/components class to base actions/components, created new empty actions/components class (Task #29)

---
+++ trunk/plugins/sfsMemberPlugin/modules/member/lib/BaseMemberComponents.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
+ * Member components.
+ *
+ * @package    plugins.sfsMemberPlugin
+ * @subpackage modules.member
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class BaseMemberComponents extends sfComponents
+{
+   /**
+    * Form for set contact info.
+    *
+    * @param  void
+    * @return void
+    * @author Dmitry Nesteruk <nest@dev-zp.com>
+    * @access public
+    */
+    public function executeContactForm()
+    {
+        $this->member = $this->getUser()->getUser();
+        $this->form = new sfsMemberContactForm($this->member);
+    }
+    
+   /**
+    * Contact info.
+    *
+    * @param  void
+    * @return void
+    * @author Dmitry Nesteruk <nest@dev-zp.com>
+    * @access public
+    */
+    public function executeContactInfo()
+    {
+        $this->member = $this->getUser()->getUser();
+        
+        $this->info = array(
+            'primary_phone'    => $this->member->getPrimaryPhone(),
+            'secondary_phone'  => $this->member->getSecondaryPhone()
+        );
+    }
+}
