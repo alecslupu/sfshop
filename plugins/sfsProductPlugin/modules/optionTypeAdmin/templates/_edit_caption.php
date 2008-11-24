800a5b263075ea2f0cf94d681e9099a3012ec0fc
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-24 11:56:40 +0000

    modified generator for optionTypeAdmin module, added validate schema for optionTypeAdmin module

--- plugins/sfsProductPlugin/modules/optionTypeAdmin/templates/_edit_caption.php
+++ plugins/sfsProductPlugin/modules/optionTypeAdmin/templates/_edit_caption.php
@@ -1,5 +1,5 @@
 <?php if ($option_type->isNew()): ?>
     Add new option
 <?php else: ?>
-    Edit option &ldquo; <?php echo $option_type->getTitle() ?> &rdquo;";
+    Edit option &ldquo; <?php echo $option_type->getTitle() ?> &rdquo;
 <?php endif; ?>
