1fd0592d00f453ff0d57b52f6b12b15526aaeabe
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-24 10:17:01 +0000

    moved actions/categories to base classes for categoryAdmin module

--- plugins/sfsCategoryPlugin/modules/categoryAdmin/actions/actions.class.php
+++ plugins/sfsCategoryPlugin/modules/categoryAdmin/actions/actions.class.php
@@ -9,96 +9,17 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require_once(dirname(__FILE__).'/../lib/BaseCategoryAdminActions.class.php');
+
 /**
- * categoryAdmin actions.
+ * Category admin actions.
  *
  * @package    plugins.sfsCategoryPlugin
  * @subpackage modules.categoriesAdmin
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
  */
-class categoryAdminActions extends autocategoryAdminActions
+class categoryAdminActions extends BaseCategoryAdminActions
 {
-    public function executeCatalogList()
-    {
-        sfLoader::loadHelpers('sfsCategory');
-        
-        $category = CategoryPeer::retrieveByPK($this->getRequestParameter('id'));
-        $path = $category->getPath();
-        $this->redirect('catalogAdmin/list?path=' . generate_category_path_for_url($path));
-    }
-    
-    public function executeDelete()
-    {
-        sfLoader::loadHelpers('sfsCategory');
-        
-        if ($this->hasRequestParameter('id')) {
-            $category = CategoryPeer::retrieveByPK($this->getRequestParameter('id'));
-            $this->forward404Unless($category);
-            
-            $category->setIsDeleted(1);
-            $category->save();
-            
-            $this->clearFrontendCache();
-            
-            $ids = array();
-            $ids[] = $category->getId();
-            
-            $child = CategoryPeer::getAllChild($category->getId());
-            
-            if (count($child) > 0) {
-                foreach ($child as $category) {
-                    $category->setIsDeleted(1);
-                    $category->save();
-                    
-                    $ids[] = $category->getId();
-                }
-            }
-            
-            $productsIds = Product2CategoryPeer::getProductsIdsByCategoriesIds($ids);
-            
-            ProductPeer::deleteByIds($productsIds);
-            
-            $path = $category->getCategoryRelatedByParentId()->getPath();
-            
-            $this->redirect('catalogAdmin/list?path=' . generate_category_path_for_url($path));
-        }
-    }
-    
-    public function executeDeleteThumbnail()
-    {
-        ThumbnailPeer::deleteByAssetIdAndAssetTypeModel($this->getRequestParameter('id'), 'Category');
-        $this->redirect('categoryAdmin/edit?id=' . $this->getRequestParameter('id'));
-    }
-    
-    protected function saveCategory($category)
-    {
-        sfLoader::loadHelpers('sfsCategory');
-        $category->save();
-        
-        if ($this->getRequest()->hasFile('category[thumbnail]')) {
-            $originalThumbnail = ThumbnailPeer::generate($category->getId(), sfConfig::get('app_category_thumbnails_dir_name'), 'Category');
-            $fileInfo = sfsThumbnailUtil::uploadFile('category[thumbnail]', $originalThumbnail->getStoragePath());
-            
-            if ($fileInfo !== null) {
-                $thumbnailMime = ThumbnailMimePeer::retrieveByMime($fileInfo['mime']);
-                
-                $originalThumbnail->setMimeExtension(str_replace('.', '', $fileInfo['extension']));
-                $originalThumbnail->setMimeId($thumbnailMime->getId());
-                $originalThumbnail->save();
-                
-                sfsThumbnailUtil::convert();
-            }
-        }
-        
-        if ($category->getCategoryRelatedByParentId()) {
-            $path = $category->getCategoryRelatedByParentId()->getPath();
-            $redirectTo = 'catalogAdmin/list?path=' . generate_category_path_for_url($path);
-        }
-        else {
-            $redirectTo = 'catalogAdmin/list';
-        }
-        
-        $this->redirect($redirectTo);
-    }
+
 }
