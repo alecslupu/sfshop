831ddc7db7e1d689672cd55c3f0631d56bc34eb1
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-24 12:11:27 +0000

    moved actions to base classes for optionValueAdmin module

--- plugins/sfsProductPlugin/modules/optionValueAdmin/actions/actions.class.php
+++ plugins/sfsProductPlugin/modules/optionValueAdmin/actions/actions.class.php
@@ -1,69 +1,25 @@
 <?php
 
 /**
- * optionValueAdmin actions.
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+require_once(dirname(__FILE__).'/../lib/BaseOptionValueAdminActions.class.php');
+
+/**
+ * OptionValue admin actions.
  *
- * @package    sfShop
- * @subpackage optionValueAdmin
- * @author     Dmitry Nesteruk
+ * @package    plugins.sfsProductPlugin
+ * @subpackage modules.optionValueAdmin
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
  */
 class optionValueAdminActions extends autooptionValueAdminActions
 {
-    public function executeDelete()
-    {
-        if ($this->hasRequestParameter('id')) {
-            $option = OptionValuePeer::retrieveByPK($this->getRequestParameter('id'));
-            $this->forward404Unless($option);
-            
-            $option->setIsDeleted(1);
-            $option->save();
-            
-            $this->redirect('optionValueAdmin/list');
-        }
-    }
-    
-    public function handleErrorEdit()
-    {
-        $request = $this->getRequest();
-        
-        if ($request->isXmlHttpRequest()) {
-            
-            $errors = array();
-            
-            foreach ($request->getErrors() as $key => $error) {
-                
-                $matches = '';
-                preg_match('/option_value\{(.*?)\}/', $key, $matches);
-                
-                $errors[$matches[1]] = $error;
-            }
-            
-            return $this->renderText(sfsJSONPeer::createResponseError($errors));
-        }
-        else {
-            return parent::handleErrorEdit();
-        }
-    }
-    
-    public function handlePost($type)
-    {
-        $request = $this->getRequest();
-        
-        if ($request->isXmlHttpRequest()) {
-            $this->updateOptionValueFromRequest();
-            
-            $this->saveOptionValue($this->option_value);
-            
-            $response = array(
-                'id'    => $this->option_value->getId(),
-                'title' => $this->option_value->getTitle()
-            );
-            
-            return $this->renderText(sfsJSONPeer::createResponseSuccess($response));
-        }
-        else {
-            parent::handlePost($type);
-        }
-    }
+
 }
