74335bf180b44492c1e3dad647ec3c0a48666471
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-24 12:03:16 +0000

    moved actions to base classes for optionTypeAdmin module

--- trunk/plugins/sfsProductPlugin/modules/optionTypeAdmin/actions/actions.class.php
+++ trunk/plugins/sfsProductPlugin/modules/optionTypeAdmin/actions/actions.class.php
@@ -1,36 +1,25 @@
 <?php
 
 /**
- * optionTypeAdmin actions.
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+require_once(dirname(__FILE__).'/../lib/BaseOptionTypeAdminActions.class.php');
+
+/**
+ * OptionType admin actions.
  *
- * @package    sfShop
- * @subpackage optionTypeAdmin
- * @author     Dmitry Nesteruk
+ * @package    plugins.sfsProductPlugin
+ * @subpackage modules.optionTypeAdmin
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
  */
-class optionTypeAdminActions extends autooptionTypeAdminActions
+class optionTypeAdminActions extends BaseOptionTypeAdminActions
 {
-    public function executeDelete()
-    {
-        if ($this->hasRequestParameter('id')) {
-            $option = OptionTypePeer::retrieveByPK($this->getRequestParameter('id'));
-            $this->forward404Unless($option);
-            
-            $option->setIsDeleted(1);
-            $option->save();
-            
-            $this->redirect('optionTypeAdmin/list');
-        }
-    }
-    
-    public function executeValuesList()
-    {
-        sfLoader::loadHelpers('Url');
-        $this->redirect(url_for('optionValueAdmin/list', true) . '?filters[type_id]=' . $this->getRequestParameter('id') . '&filter=filter');
-    }
-    
-    protected function addFiltersCriteria($c)
-    {
-        parent::addFiltersCriteria($c);
-    }
+
 }
