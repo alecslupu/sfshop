2b4175c76f8c93a40a508631f039353ef86a6712
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-26 10:11:19 +0000

    added component class for paymentAdmin module

---
+++ trunk/plugins/sfsPaymentPlugin/modules/paymentAdmin/actions/components.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+require_once(dirname(__FILE__).'/../lib/BasePaymentAdminComponents.class.php');
+
+/**
+ * Payment components.
+ *
+ * @package    plugins.sfsPaymentPlugin
+ * @subpackage modules.paymentAdmin
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ */
+class paymentAdminComponents extends BasePaymentAdminComponents
+{
+
+}
