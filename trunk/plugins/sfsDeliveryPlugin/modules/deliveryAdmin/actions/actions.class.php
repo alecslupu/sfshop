aa7053b10e6b07de1ca01b78f39e42361ff53f40
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-11-26 10:09:23 +0000

    moved actions/components to base classes, added new component for viewing delivery service details on order details page

--- trunk/plugins/sfsDeliveryPlugin/modules/deliveryAdmin/actions/actions.class.php
+++ trunk/plugins/sfsDeliveryPlugin/modules/deliveryAdmin/actions/actions.class.php
@@ -9,6 +9,8 @@
  * For the full copyright and license information, please view the LICENSE file.
  */
 
+require_once(dirname(__FILE__).'/../lib/BasePaymentAdminActions.class.php');
+
 /**
  * deliveryAdmin actions.
  *
@@ -17,89 +19,7 @@
  * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
  */
-class deliveryAdminActions extends autodeliveryAdminActions
+class deliveryAdminActions extends BaseDeliveryAdminActions
 {
-    public function executeEdit()
-    {
-        $request = $this->getRequest();
-        
-        $criteria = new Criteria();
-        DeliveryPeer::addAdminCriteria($criteria);
-        $this->delivery = DeliveryPeer::retrieveById($request->getParameter('id'), $criteria);
-        $this->forward404Unless($this->delivery);
-        
-        $this->form = new DeliveryForm();
-        $this->form->setDefault('title', $this->delivery->getTitle());
-        $this->form->setDefault('description', $this->delivery->getDescription());
-        $this->form->setDefault('is_active', $this->delivery->getIsActive());
-        
-        $params = sfsJSONPeer::decode($this->delivery->getParams());
-        
-        $className = $this->delivery->getNameClassFormParams();
-        $formParams = new $className();
-        
-        $this->form->setDefault('id', $request->getParameter('id'));
-        
-        foreach ($params as $key => $value) {
-            $formParams->setDefault($key, $value);
-        }
-        
-        $this->labels = $formParams->getWidgetSchema()->getLabels();
-        $this->form->embedForm('params', $formParams);
-        
-        if ($this->getRequest()->isMethod('post')) {
-            
-            $data = $request->getParameter('data');
-            $this->form->bind($data);
-            
-            if ($this->form->isValid()) {
-                
-                foreach ($params as $key => $value) {
-                    if (!isset($data['params'][$key]) && !is_bool($value)) {
-                        $data['params'][$key] = $value;
-                    }
-                    else {
-                        if (isset($data['params'][$key]) && $data['params'][$key] == 'on' && is_bool($value)) {
-                            $data['params'][$key] = true;
-                        }
-                        else if(!isset($data['params'][$key]) && is_bool($value)) {
-                            $data['params'][$key] = false;
-                        }
-                    }
-                }
-                
-                $params = sfsJSONPeer::encode($data['params']);
-                
-                $this->delivery->setTitle($data['title']);
-                $this->delivery->setDescription($data['description']);
-                
-                if (isset($data['is_active'])) {
-                    $this->delivery->setIsActive(true);
-                }
-                else {
-                    $this->delivery->setIsActive(false);
-                }
-                
-                $acceptCurrenciesCodes = $this->delivery->getAcceptCurrenciesCodes();
-                $criteria = new Criteria();
-                CurrencyPeer::addPublicCriteria($criteria);
-                $isExist = CurrencyPeer::checkExistenceByCodes(explode(',', $acceptCurrenciesCodes), $criteria);
-                
-                if (!$isExist) {
-                    $this->getUser()->setFlash('notice', 'Your modifications have been saved, 
-                        but this service is inactive, because your store does not use currencies which accept this service.
-                        This service is accept following currencies: ' . str_replace(',', ', ', $acceptCurrenciesCodes));
-                    $this->delivery->setIsActive(false);
-                }
-                else {
-                    $this->getUser()->setFlash('notice', 'Your modifications have been saved');
-                }
-                
-                $this->delivery->setParams($params);
-                $this->delivery->save();
-                
-                $this->redirect('deliveryAdmin/list');
-            }
-        }
-    }
+
 }
