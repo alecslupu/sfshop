eac1b5316517c5f38cda4827e43aad5bddc69b6a
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-03 11:14:47 +0000

    added fixtures with member secret questions

--- plugins/sfsInstallPlugin/modules/installer/lib/sfsConfigureForm.class.php
+++ plugins/sfsInstallPlugin/modules/installer/lib/sfsConfigureForm.class.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
+ * Select delivery service form.
+ *
+ * @package    plugin.sfsInstallPlugin
+ * @subpackage modules.installer
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
 class sfsConfigureForm extends sfForm
 {
     public function configure()
@@ -15,6 +32,8 @@ class sfsConfigureForm extends sfForm
         
         $this->setDefault('database_host', 'localhost');
         
+        $this->getWidgetSchema()->setHelp('database_name', 'Database must be exists before continue installation.');
+        
         $validatorDatabaseHost = new sfValidatorString(
             array('required' => true),
             array('required' => 'Database host is a required field')
@@ -25,11 +44,16 @@ class sfsConfigureForm extends sfForm
             array('required' => 'Database name is a required field')
         );
         
+        $validatorDatabaseUsername = new sfValidatorString(
+            array('required' => true),
+            array('required' => 'Database username is a required field')
+        );
+        
         $this->setValidators(
             array(
                 'database_host'     => $validatorDatabaseHost,
                 'database_name'     => $validatorDatabaseName,
-                'database_username' => new sfValidatorString(),
+                'database_username' => $validatorDatabaseUsername,
                 'database_password' => new sfValidatorString(array('required' => false))
             )
         );
