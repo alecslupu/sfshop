c032b9d0162450416d9d870af65aec64adcce0cf
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-08 10:39:07 +0000

    added posibility to edit currency (issue 13)

--- trunk/plugins/sfsCurrencyPlugin/modules/currencyAdmin/actions/actions.class.php
+++ trunk/plugins/sfsCurrencyPlugin/modules/currencyAdmin/actions/actions.class.php
@@ -1,13 +1,24 @@
 <?php
 
 /**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+require_once(dirname(__FILE__).'/../lib/BaseCurrencyAdminActions.class.php');
+
+/**
  * currencyAdmin actions.
  *
- * @package    sfShop
- * @subpackage currencyAdmin
- * @author     Your name here
+ * @package    pugins.sfsCurrencyPlugin
+ * @subpackage modules.currencyAdmin
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
  * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
  */
-class currencyAdminActions extends autocurrencyAdminActions
+class currencyAdminActions extends BaseCurrencyAdminActions
 {
 }
---
+++ trunk/plugins/sfsCurrencyPlugin/modules/currencyAdmin/lib/BaseCurrencyAdminActions.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
+ * Base currencyAdmin actions.
+ *
+ * @package    pugins.sfsCurrencyPlugin
+ * @subpackage modules.currencyAdmin
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class BaseCurrencyAdminActions extends autocurrencyAdminActions
+{
+    protected function deleteCurrency($currency)
+    {
+        if ($currency->getIsDefault()) {
+            $this->getRequest()->setError('delete', 'You can not delete this currency, bacause it is a default currency, you can rename it only.');
+            return $this->forward('currencyAdmin', 'list');
+        }
+        else {
+            parent::deleteCurrency($currency);
+        }
+    }
+}
