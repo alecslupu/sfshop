4917abb278a660a51e27db4f34619b5284799b16
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-09 10:50:18 +0000

    created languageAdmin module (issue 28)

--- plugins/sfsCorePlugin/lib/model/common/Language.php
+++ plugins/sfsCorePlugin/lib/model/common/Language.php
@@ -1,6 +1,15 @@
 <?php
 
 /**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
  * Subclass for representing a row from the 'languages' table.
  *
  * 
@@ -9,4 +18,34 @@
  */ 
 class Language extends BaseLanguage
 {
+   /**
+    * Gets path to icon on a filesystem.
+    * 
+    * @param  void
+    * @return string
+    * @author Dmitry Nesteruk <nest@dev-zp.com>
+    * @access public
+    */
+    public function getIconPath()
+    {
+        return sfConfig::get('sf_web_dir') 
+            . '/images/' . sfConfig::get('app_languages_images_dir') . '/' 
+            . strtolower(str_replace(' ', '', $this->getTitleEnglish())) 
+            . '/icon.png';
+    }
+    
+   /**
+    * Gets url to icon for web.
+    * 
+    * @param  void
+    * @return string
+    * @author Dmitry Nesteruk <nest@dev-zp.com>
+    * @access public
+    */
+    public function getIconUrl()
+    {
+        return '/images/' . sfConfig::get('app_languages_images_dir') . '/' 
+            . strtolower(str_replace(' ', '', $this->getTitleEnglish())) 
+            . '/icon.png';
+    }
 }
---
+++ plugins/sfsCorePlugin/modules/languageAdmin/actions/actions.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nest@dev-zp.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+require_once(dirname(__FILE__).'/../lib/BaseLanguageAdminActions.class.php');
+
+/**
+ * languageAdmin actions.
+ *
+ * @package    plugin.sfsCorePlugin
+ * @subpackage modules.languageAdmin
+ * @author     Dmitry Nesteruk <nest@dev-zp.com>
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class languageAdminActions extends BaseLanguageAdminActions
+{
+}
