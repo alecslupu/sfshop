0c3edd6f0663da25bff2d08fdf5b1f44658643aa
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-11 10:54:11 +0000

    removed sfsLanguageHelper

--- plugins/sfsCorePlugin/modules/core/templates/_selectLanguage.php
+++ plugins/sfsCorePlugin/modules/core/templates/_selectLanguage.php
@@ -1,6 +1,16 @@
 <?php if ($languages !== null && count($languages) > 1): ?>
     <?php echo __('Language') ?>:
     <?php foreach ($languages as $language): ?>
-        <?php echo link_to(language_icon($language), '@core_changeLanguage?culture=' . $language->getCulture()) ?>
+        <?php echo link_to(
+            image_tag(
+                $language->getIconUrl(),
+                array(
+                    'title' => $language->getTitleOwn(),
+                    'alt'   => $language->getTitleOwn(),
+                    'align' => 'absmiddle'
+                )
+            ), 
+            '@core_changeLanguage?culture=' . $language->getCulture()
+        ) ?>
     <?php endforeach; ?>
 <?php endif; ?>
