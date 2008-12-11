d5186d8bc998c5950a486684076214e9f9eb7825
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-11 08:42:00 +0000

    renamed coreInformationAdmin on informationAdmin (issue 52)

--- plugins/sfsCorePlugin/config/config.php
+++ plugins/sfsCorePlugin/config/config.php
@@ -2,6 +2,6 @@
 
 if (sfConfig::get('sf_app') == 'backend') {
     $enabledModules = sfConfig::get('sf_enabled_modules');
-    $enabledModules = array_merge($enabledModules, array('languageAdmin'));
+    $enabledModules = array_merge($enabledModules, array('languageAdmin', 'informationAdmin'));
     sfConfig::set('sf_enabled_modules', $enabledModules);
 }
