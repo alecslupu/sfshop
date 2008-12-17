c3fa4784272e9ca98cb59244c4dc0a4b87ab144b
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-17 12:19:50 +0000

    removed validate schemas

--- trunk/plugins/sfsCorePlugin/modules/informationAdmin/templates/_edit_caption.php
+++
@@ -1,5 +0,0 @@
-<?php if ($information->isNew()): ?>
-    <?php echo __('Add new page') ?>
-<?php else: ?>
-    <?php echo __('Edit page &ldquo;%title%&rdquo;', array('%title%' => $information->getTitle())) ?>
-<?php endif; ?>
