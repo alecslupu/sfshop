0f5f3342400b858a99a61efcda16c20563097ef6
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-18 09:59:25 +0000

    rewrote currencyAdmin module (issue 84)

--- trunk/plugins/sfsCurrencyPlugin/modules/currencyAdmin/lib/BaseCurrencyAdminActions.class.php
+++ trunk/plugins/sfsCurrencyPlugin/modules/currencyAdmin/lib/BaseCurrencyAdminActions.class.php
@@ -19,14 +19,40 @@
  */
 class BaseCurrencyAdminActions extends autocurrencyAdminActions
 {
-    protected function deleteCurrency($currency)
+    public function executeDelete(sfWebRequest $request)
     {
-        if ($currency->getIsDefault()) {
-            $this->getRequest()->setError('delete', 'You can not delete this currency, bacause it is a default currency, you can rename it only.');
-            return $this->forward('currencyAdmin', 'list');
+        $request->checkCSRFProtection();
+        
+        $this->dispatcher->notify(new sfEvent($this, 'admin.delete_object', array('object' => $this->getRoute()->getObject())));
+        
+        if ($this->getRoute()->getObject()->getIsDefault()) {
+            $this->getUser()->setFlash('error', 'You can not delete currency "' . $this->getRoute()->getObject()->getTitle() . '", bacause it is a default currency, you can rename it only.');
         }
         else {
-            parent::deleteCurrency($currency);
+            $this->getUser()->setFlash('notice', 'The item was deleted successfully.');
+            $this->getRoute()->getObject()->delete();
         }
+        
+        $this->redirect('@currencyAdmin');
+    }
+    
+    protected function executeBatchDelete(sfWebRequest $request)
+    {
+        $ids = $request->getParameter('ids');
+        
+        $criteria = new Criteria();
+        $criteria->add(CurrencyPeer::ID, $ids, Criteria::IN);
+        $currencies = CurrencyPeer::getAll($criteria);
+        
+        foreach ($currencies as $currency) {
+            if ($currency->getIsDefault()) {
+                $this->getUser()->setFlash('error', 'You can not delete currency "' . $currency->getTitle() . '", bacause it is a default currency, you can rename it only.');
+            }
+            else {
+                $currency->delete();
+                $this->getUser()->setFlash('notice', 'The selected items have been deleted successfully.');
+            }
+        }
+        $this->redirect('@currencyAdmin');
     }
 }
