81253aba4225cd7e6024da41401ecec5179f47cc
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-18 12:01:10 +0000

    removed partial for caption

--- plugins/sfsCurrencyPlugin/modules/currencyAdmin/templates/_edit_caption.php
+++
@@ -1,5 +0,0 @@
-<?php if ($currency->isNew()): ?>
-    <?php echo __('Add new currency') ?>
-<?php else: ?>
-    <?php echo __('Edit currency &ldquo;%title%&rdquo;', array('%title%' => $currency->getTitle())) ?>
-<?php endif; ?>
