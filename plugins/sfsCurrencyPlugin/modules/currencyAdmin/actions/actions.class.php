a16f75d421fbf7a34c9917a7279152e886dd303f
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-18 09:59:25 +0000

    rewrote currencyAdmin module (issue 84)

--- plugins/sfsCurrencyPlugin/modules/currencyAdmin/actions/actions.class.php
+++ plugins/sfsCurrencyPlugin/modules/currencyAdmin/actions/actions.class.php
@@ -10,12 +10,14 @@
  */
 
 require_once(dirname(__FILE__).'/../lib/BaseCurrencyAdminActions.class.php');
+require_once dirname(__FILE__).'/../lib/CurrencyAdminGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/CurrencyAdminGeneratorHelper.class.php';
 
 /**
  * currencyAdmin actions.
  *
  * @package    pugins.sfsCurrencyPlugin
- * @subpackage modules.currencyAdmin
+ * @subpackage modules.currencyAdmin.lib
  * @author     Dmitry Nesteruk <nesterukd@gmail.com>
  * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
  */
