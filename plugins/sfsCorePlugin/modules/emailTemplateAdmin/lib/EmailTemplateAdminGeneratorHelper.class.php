a798c577dfd0c8c20e9b06eedcf6b91d49adb140
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-19 11:29:57 +0000

    set property keywords

--- plugins/sfsCorePlugin/modules/emailTemplateAdmin/lib/EmailTemplateAdminGeneratorHelper.class.php
+++ plugins/sfsCorePlugin/modules/emailTemplateAdmin/lib/EmailTemplateAdminGeneratorHelper.class.php
@@ -15,7 +15,7 @@
  * @package    plugin.sfsCorePlugin
  * @subpackage modules.emailTemplateAdmin.lib
  * @author     Dmitry Nesteruk <nesterukd@gmail.com>
- * @version    SVN: $Id: helper.php 12474 2008-10-31 10:41:27Z fabien $
+ * @version    SVN: $Id$
  */
 class EmailTemplateAdminGeneratorHelper extends BaseEmailTemplateAdminGeneratorHelper
 {
