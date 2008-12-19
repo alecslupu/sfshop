d25a119085ab13178942b2d9f3baeacaf89bcb8e
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-19 09:15:20 +0000

    test commit

--- trunk/plugins/sfsCorePlugin/modules/languageAdmin/actions/actions.class.php
+++ trunk/plugins/sfsCorePlugin/modules/languageAdmin/actions/actions.class.php
@@ -17,7 +17,7 @@ require_once(dirname(__FILE__).'/../lib/BaseLanguageAdminActions.class.php');
  * @package    plugin.sfsCorePlugin
  * @subpackage modules.languageAdmin
  * @author     Dmitry Nesteruk <nesterukd@gmail.com>
- * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ * @version    SVN: $Id$
  */
 class languageAdminActions extends BaseLanguageAdminActions
 {
