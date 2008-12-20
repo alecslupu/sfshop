1f8564630cddb328795dfafd057b6d86cdc7911a
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-20 10:05:54 +0000

    rewrote module administratorAdmin (issue 86)

--- trunk/plugins/sfsCorePlugin/lib/form/admin/AdminForm.class.php
+++ trunk/plugins/sfsCorePlugin/lib/form/admin/AdminForm.class.php
@@ -1,15 +1,88 @@
 <?php
 
 /**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nesterukd@gmail.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
  * Admin form.
  *
- * @package    form
- * @subpackage admin
+ * @package    plugin.sfsCorePlugin
+ * @subpackage lib.form
+ * @author     Dmitry Nesteruk <nesterukd@gmail.com>
  * @version    SVN: $Id: sfPropelFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
  */
 class AdminForm extends BaseAdminForm
 {
-  public function configure()
-  {
-  }
+    public function configure()
+    {
+        $arrayCredentials = array('superadmin' => 'superadmin', 'admin' => 'admin');
+        
+        $this->setWidgets(array(
+            'id'          => new sfWidgetFormInputHidden(),
+            'credential'  => new sfWidgetFormSelect(array('choices' => $arrayCredentials)),
+            'email'       => new sfWidgetFormInput(),
+            'first_name'  => new sfWidgetFormInput(),
+            'last_name'   => new sfWidgetFormInput(),
+            'is_active'   => new sfWidgetFormInputCheckbox()
+        ));
+        
+        $validatorCredential = new sfValidatorChoice(
+            array('choices'  => array_keys($arrayCredentials)),
+            array('invalid'  => 'Please select a credential')
+        );
+        
+        $validatorEmail = new sfValidatorAnd(
+            array(
+                new sfValidatorEmail(
+                    array('required' => true),
+                    array('invalid'  => 'This is not a valid email address')
+                ),
+                new sfsValidatorAdmin(
+                    array('check_unique_email' => true),
+                    array('check_unique_email' => 'An account with this email already exists')
+                )
+            ),
+            array('required' => true),
+            array('required' => 'Email is a required field')
+        );
+        
+        $validatorFirstName = new sfValidatorString(
+            array(
+                'required'   => true,
+                'max_length' => 255
+            ),
+            array(
+                'required'   => 'First Name is a required field',
+                'max_length' => 'First Name can not be more 255 characters',
+            )
+        );
+        
+        $validatorLastName = new sfValidatorString(
+            array(
+                'required'   => true,
+                'max_length' => 255
+            ),
+            array(
+                'required'   => 'Last Name is a required field',
+                'max_length' => 'Last Name can not be more 255 characters',
+            )
+        );
+        
+        $this->setValidators(array(
+          'id'          => new sfValidatorPropelChoice(array('model' => 'Admin', 'column' => 'id', 'required' => false)),
+          'credential'  => $validatorCredential,
+          'email'       => $validatorEmail,
+          'first_name'  => $validatorFirstName,
+          'last_name'   => $validatorLastName,
+          'is_active'   => new sfValidatorBoolean()
+        ));
+        
+        $this->getWidgetSchema()->setNameFormat('admin[%s]');
+    }
 }
--- trunk/plugins/sfsCorePlugin/modules/administratorAdmin/templates/changeMyPasswordSuccess.php
+++ trunk/plugins/sfsCorePlugin/modules/administratorAdmin/templates/changeMyPasswordSuccess.php
@@ -1,72 +1,39 @@
-<?php use_helper('Validation', 'I18N', 'Date') ?>
+<?php use_helper('I18N', 'Date', 'Form') ?>
+<?php include_stylesheets_for_form($form) ?>
+<?php include_javascripts_for_form($form) ?>
 
-<?php use_stylesheet('/sf/sf_admin/css/main') ?>
+<?php include_partial('administratorAdmin/assets') ?>
 
 <div id="sf_admin_container">
+  <h1><?php echo __('Change my pasaword') ?></h1>
 
-    <h1>Change my password</h1>
+  <?php include_partial('administratorAdmin/flashes') ?>
+
+  <div id="sf_admin_content">
+    <div class="sf_admin_form">
+      <?php echo form_tag('@administratorAdmin_changeMyPassword') ?>
+        <?php echo $form->renderHiddenFields() ?>
     
-    <?php if ($sf_user->hasFlash('notice')): ?>
-        <div class="save-ok">
-           <h2><?php echo __($sf_user->getFlash('notice')) ?></h2>
-        </div>
-    <?php endif; ?>
-    
-    <div id="sf_admin_content">
+        <?php if ($form->hasGlobalErrors()): ?>
+          <?php echo $form->renderGlobalErrors() ?>
+        <?php endif; ?>
         
-        <?php echo form_tag('administratorAdmin/changeMyPassword', array(
-            'id'        => 'sf_admin_change_password_form',
-            'name'      => 'sf_admin_change_password_form',
-            'multipart' => true,
-            'method'    => 'post'
-        )); ?>
+        <fieldset id="sf_fieldset">
+            <?php foreach (array('current_password', 'password', 'confirm_password') as $name): ?>
+              <div class="sf_admin_form_row sf_admin_text sf_admin_form_field <?php $form[$name]->hasError() and print ' errors' ?>">
+                <?php echo $form[$name]->renderError() ?>
+                <div>
+                  <?php echo $form[$name]->renderLabel() ?>
+                  <?php echo $form[$name]->render() ?>
+                </div>
+              </div>
+            <?php endforeach; ?>
+        </fieldset>
         
-            <fieldset id="sf_fieldset_none" class="">
-            
-            <div class="form-row">
-              <?php echo label_for('admin[current_password]', __('Current password') . ':') ?>
-              <div class="content<?php if ($sf_request->hasError('admin{current_password}')): ?> form-error<?php endif; ?>">
-                  <?php if ($sf_request->hasError('admin{current_password}')): ?>
-                      <?php echo form_error('admin{current_password}', array('class' => 'form-error-msg')) ?>
-                  <?php endif; ?>
-                
-                  <?php echo input_password_tag('admin[current_password]', '', array ('size' => 30)); ?>
-              </div>
-            </div>
-            
-            <div class="form-row">
-              <?php echo label_for('admin[password]', __('New password') . ':') ?>
-              <div class="content<?php if ($sf_request->hasError('admin{password}')): ?> form-error<?php endif; ?>">
-                <?php if ($sf_request->hasError('admin{password}')): ?>
-                    <?php echo form_error('admin{password}', array('class' => 'form-error-msg')) ?>
-                <?php endif; ?>
-                
-                <?php echo input_password_tag('admin[password]', '', array ('size' => 30)); ?>
-              </div>
-            </div>
-            
-            <div class="form-row">
-              <?php echo label_for('admin[confirm_password]', __('Confirm password') . ':') ?>
-              <div class="content<?php if ($sf_request->hasError('admin{confirm_password}')): ?> form-error<?php endif; ?>">
-                <?php if ($sf_request->hasError('admin{confirm_password}')): ?>
-                    <?php echo form_error('admin{confirm_password}', array('class' => 'form-error-msg')) ?>
-                <?php endif; ?>
-                
-                <?php echo input_password_tag('admin[confirm_password]', '', array ('size' => 30)); ?>
-              </div>
-            </div>
-            
-            </fieldset>
-            
-            <ul class="sf_admin_actions">
-                <li><?php echo submit_tag(__('change'), array (
-              'name' => 'save',
-              'class' => 'sf_admin_action_save',
-            )) ?></li>
-            </ul>
-        
-        </form>
-    
+        <ul class="sf_admin_actions">
+            <li class="sf_admin_action_save"><input type="submit" value="<?php echo __('Save') ?>" /></li>
+        </ul>
+      </form>
     </div>
-
+  </div>
 </div>
