82671e39aaf133bc7c53284b50d6d1f5c7e66187
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-22 21:39:45 +0000

    removed old template

--- trunk/plugins/sfsMemberPlugin/modules/memberAdmin/templates/_edit_caption.php
+++
@@ -1,5 +0,0 @@
-<?php if ($member->isNew()): ?>
-    <?php echo __('Add new member') ?>
-<?php else: ?>
-    <?php echo __('Edit member &ldquo;%title%&rdquo;', array('%title%' => $member->getFirstName() . ' ' . $member->getLastName())) ?>
-<?php endif; ?>
