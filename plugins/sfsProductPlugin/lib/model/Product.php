a8dbaefec068a0302f55227c68fc29e626df2940
revollat <revollat@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-23 14:18:41 +0000

    added __toString() method for admin generator

--- plugins/sfsProductPlugin/lib/model/Product.php
+++ plugins/sfsProductPlugin/lib/model/Product.php
@@ -3,17 +3,17 @@
 /**
  * Subclass for representing a row from the 'products' table.
  *
- * 
+ *
  *
  * @package plugins.sfsProductPlugin.lib.model
- */ 
+ */
 class Product extends BaseProduct
 {
     protected $hasOptions = null;
-    
+
    /**
     * Gets thumbnail object for current product.
-    * 
+    *
     * @param  string $type
     * @return object
     * @author Dmitry Nesteruk
@@ -23,10 +23,10 @@ class Product extends BaseProduct
     {
         return ThumbnailPeer::retrieveByTypeAndAssetId($thumbnailType, $this->getId(), get_class($this));
     }
-    
+
    /**
     * Gets brand optin for product with translation.
-    * 
+    *
     * @param  object $con
     * @return object
     * @author Dmitry Nesteruk
@@ -35,7 +35,7 @@ class Product extends BaseProduct
     public function getBrand(PropelPDO $con = null)
     {
         if ($this->getBrandId() !== null) {
-            
+
             $criteria = new Criteria();
             $criteria->add(BrandPeer::ID, $this->getBrandId());
             list($brand) = BrandPeer::doSelectWithTranslation($criteria);
@@ -45,32 +45,37 @@ class Product extends BaseProduct
             return null;
         }
     }
-    
+
     public function setCategoryId($categoryId)
     {
         $this->save();
-        
+
         $c = new Criteria();
         $c->addAnd(Product2CategoryPeer::PRODUCT_ID, $this->getId(), Criteria::EQUAL);
         foreach (Product2CategoryPeer::doSelect($c) as $p2c) {
             $p2c->delete();
         }
-        
+
         $p2c = new Product2Category();
         $p2c->setProductId($this->getId());
         $p2c->setCategoryId($categoryId);
         $p2c->save();
     }
-    
+
     public function getCategoryId()
     {
         $c = new Criteria();
         $c->addAnd(Product2CategoryPeer::PRODUCT_ID, $this->getId(), Criteria::EQUAL);
-        
+
         if (($p2c = Product2CategoryPeer::doSelectOne($c)) !== null) {
             return $p2c->getCategoryId();
         }
         return null;
+    }
+
+    public function __toString()
+    {
+    	return $this->getTitle();
     }
 }
 
