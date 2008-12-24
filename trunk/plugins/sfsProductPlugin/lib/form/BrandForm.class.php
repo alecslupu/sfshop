43b930de56892b806b428c7b52d49a2f2e676f36
nesterukd <nesterukd@e1a02840-6048-0410-8644-cfba31147e63>
2008-12-24 09:53:15 +0000

    rewrote brandAdmin module (issue 92)

--- trunk/plugins/sfsProductPlugin/lib/form/BrandForm.class.php
+++ trunk/plugins/sfsProductPlugin/lib/form/BrandForm.class.php
@@ -1,15 +1,48 @@
 <?php
 
 /**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nesterukd@gmail.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
  * Brand form.
  *
- * @package    form
- * @subpackage brand
+ * @package    plugin.sfsProductPlugin
+ * @subpackage lib.form
+ * @author     Dmitry Nesteruk <nesterukd@gmail.com>
  * @version    SVN: $Id$
  */
 class BrandForm extends BaseBrandForm
 {
-  public function configure()
-  {
-  }
+    public function configure()
+    {
+        parent::configure();
+        
+        $this->setWidget('url', new sfWidgetFormInput(array(), array('size' => 80)));
+        
+        $validatorUrl = new sfValidatorUrl(
+            array('required' => false),
+            array('invalid'  => 'This is not a valid url')
+        );
+        
+        $this->setValidator('url', $validatorUrl);
+        
+        $languages = LanguagePeer::getAllPublic();
+        $cultures = array();
+        
+        foreach ($languages as $language) {
+            $cultures[] = $language->getCulture();
+        }
+        
+        $this->embedI18n($cultures);
+        
+        foreach ($languages as $language) {
+            $this->getWidgetSchema()->setLabel($language->getCulture(), $language->getTitleEnglish());
+        }
+    }
 }
--- trunk/plugins/sfsProductPlugin/lib/form/BrandI18nForm.class.php
+++ trunk/plugins/sfsProductPlugin/lib/form/BrandI18nForm.class.php
@@ -1,15 +1,56 @@
 <?php
 
 /**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nesterukd@gmail.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
  * BrandI18n form.
  *
- * @package    form
- * @subpackage brand_i18n
+ * @package    plugin.sfsProductPlugin
+ * @subpackage lib.form
+ * @author     Dmitry Nesteruk <nesterukd@gmail.com>
  * @version    SVN: $Id$
  */
 class BrandI18nForm extends BaseBrandI18nForm
 {
-  public function configure()
-  {
-  }
+    public function configure()
+    {
+        parent::configure();
+        
+        $widgetDescription = new sfWidgetFormTextarea(
+            array(),
+            array('cols' => 80, 'rows' => 5)
+        );
+        
+        $this->setWidget('title', new sfWidgetFormInput(array(), array('size' => 80)));
+        $this->setWidget('description', $widgetDescription);
+        
+        $validatorTitle = new sfValidatorString(
+            array(
+                'required'   => true,
+                'max_length' => 255
+            ),
+            array(
+                'required'   => 'Title is a required field',
+                'max_length' => 'Title can not be more 255 characters'
+            )
+        );
+        
+        $validatorDescription = new sfValidatorString(
+            array(
+                'required'   => false,
+                'max_length' => 300
+            ),
+            array('max_length' => 'Description can not be more 300 characters')
+        );
+        
+        $this->setValidator('title', $validatorTitle);
+        $this->setValidator('description', $validatorDescription);
+    }
 }
