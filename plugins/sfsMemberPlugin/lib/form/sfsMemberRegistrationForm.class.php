7e82b6dfb8ff7d8c6ffc01ad081609b631620c7b
revollat <revollat@e1a02840-6048-0410-8644-cfba31147e63>
2009-01-06 14:57:59 +0000

    replace 'equal' with sfValidatorSchemaCompare::EQUAL to fix the thrown exception

--- plugins/sfsMemberPlugin/lib/form/sfsMemberRegistrationForm.class.php
+++ plugins/sfsMemberPlugin/lib/form/sfsMemberRegistrationForm.class.php
@@ -3,9 +3,9 @@
 /**
  * sfShop, open source e-commerce solutions.
  * (c) 2008 Dmitry Nesteruk <nesterukd@gmail.com>
- * 
+ *
  * Released under the MIT License.
- * 
+ *
  * For the full copyright and license information, please view the LICENSE file.
  */
 
@@ -22,26 +22,26 @@ class sfsMemberRegistrationForm extends MemberForm
     public function configure()
     {
         parent::configure();
-        
+
         $this->offsetUnset('primary_phone');
         $this->offsetUnset('secondary_phone');
         $this->offsetUnset('is_active');
-        
+
         $arrayQuestions = array();
         $questions = MemberSecretQuestionPeer::getAll();
-        
+
         if ($questions !== null) {
             $arrayQuestions[] = '';
             foreach ($questions as $question) {
                 $arrayQuestions[$question->getQuestion()] = $question->getQuestion();
             }
         }
-        
+
         $this->setWidget('password', new sfWidgetFormInputPassword());
         $this->setWidget('confirm_password', new sfWidgetFormInputPassword());
         $this->setWidget('secret_question', new sfWidgetFormSelect(array('choices' => $arrayQuestions)));
         $this->setWidget('secret_answer', new sfWidgetFormInput());
-        
+
         $this->getWidgetSchema()->setHelps(
             array(
                 'email'         => 'You will use email address for login',
@@ -49,7 +49,7 @@ class sfsMemberRegistrationForm extends MemberForm
                 'primary_phone' => 'In some urgent cases we\'ll need to contact you quickly and directly.'
             )
         );
-        
+
         $validatorPassword = new sfValidatorString(
             array(
                 'required'   => true,
@@ -62,27 +62,27 @@ class sfsMemberRegistrationForm extends MemberForm
                 'max_length' => 'Password can not be more 200 characters'
             )
         );
-        
+
         $validatorConfirmPassword = new sfValidatorString(
             array('required' => true),
             array('required' => 'Confirm password is a required field')
         );
-        
+
         $validatorComparePasswords = new sfValidatorSchemaCompare(
-            'password', 
-            'equal',
+            'password',
+            sfValidatorSchemaCompare::EQUAL,
             'confirm_password',
             array(),
             array('invalid' => 'Passwords do not match')
         );
-        
+
         unset($arrayQuestions[0]);
-        
+
         $validatorSecretQuestion = new sfValidatorChoice(
             array('choices'  => array_keys($arrayQuestions)),
             array('invalid'  => 'Please select a secret question')
         );
-        
+
         $validatorSecretAnswer = new sfValidatorString(
             array(
                 'required'   => true,
@@ -93,12 +93,12 @@ class sfsMemberRegistrationForm extends MemberForm
                 'min_length' => 'Secret answer can not be less 2 characters'
             )
         );
-        
+
         $this->setValidator('password', $validatorPassword);
         $this->setValidator('confirm_password', $validatorConfirmPassword);
         $this->setValidator('secret_question', $validatorSecretQuestion);
         $this->setValidator('secret_answer', $validatorSecretAnswer);
-        
+
         $this->validatorSchema->setPostValidator($validatorComparePasswords);
     }
 }
