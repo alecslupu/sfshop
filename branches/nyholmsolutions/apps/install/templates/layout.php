1bd58f0cbeb6e2c6fadfc9238cbd8156b8069899
nyholmsolutions <nyholmsolutions@e1a02840-6048-0410-8644-cfba31147e63>
2009-02-18 07:34:19 +0000

    moved files into plugin and other related changes third commit

--- branches/nyholmsolutions/apps/install/templates/layout.php
+++ branches/nyholmsolutions/apps/install/templates/layout.php
@@ -22,7 +22,7 @@
 ?>
 
     <table width="100%" height="100%" cellspacing="0" cellpadding="0" border="0">
-        <tr><td style="background: url(/install/images/m13.gif) repeat-x" height="106"></td></tr>
+        <tr><td style="background: url(<?php echo sfConfig::get('app_sfshop_install_images_dir')?>m13.gif) repeat-x" height="106"></td></tr>
         <tr><td height="100%" valign="top" align="center">
             <table cellspacing="0" cellpadding="0" align="center" width="700">
                 <tr><td>
@@ -30,7 +30,7 @@
                 </td></tr>
             </table>
         </td></tr>
-        <tr><td style="background: url(/install/images/m14.gif)" height="41"></td></tr>
+        <tr><td style="background: url(<?php echo sfConfig::get('app_sfshop_install_images_dir')?>images/m14.gif)" height="41"></td></tr>
     </table>
 </body>
 </html>
