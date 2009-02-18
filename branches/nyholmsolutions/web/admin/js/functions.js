d3390b7cdc7551357ccd24a9d1b6c707007956ec
nyholmsolutions <nyholmsolutions@e1a02840-6048-0410-8644-cfba31147e63>
2009-02-18 07:30:34 +0000

    moved files into plugin and other related changes few bug fixes second commit

--- branches/nyholmsolutions/web/admin/js/functions.js
+++
@@ -1,7 +0,0 @@
-function highlightFieldsWithError()
-{
-    var elements = $('sf_admin_content').select('div.form-error');
-    elements.each(function(element) {
-        element.up('div.content').addClassName('form-error');
-    });
-}
