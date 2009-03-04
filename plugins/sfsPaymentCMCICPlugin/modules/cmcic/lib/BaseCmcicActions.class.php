d76d94032ee87efebfd58c47e9c49e366d7d1b0c
revollat <revollat@e1a02840-6048-0410-8644-cfba31147e63>
2009-03-04 15:58:45 +0000

    adding CM-CIC payment support to sfShop (http://www.cmcicpaiement.fr/fr/index.html)

---
+++ plugins/sfsPaymentCMCICPlugin/modules/cmcic/lib/BaseCmcicActions.class.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * sfShop, open source e-commerce solutions.
+ * (c) 2008 Dmitry Nesteruk <nesterukd@gmail.com>
+ * 
+ * Released under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE file.
+ */
+
+/**
+ * cmcic actions.
+ *
+ * @package    plugins.sfsPaymentCMCICPlugin
+ * @subpackage modules.cmcic
+ * @author     Olivier Revollat <revollat@gmail.com>
+ */
+class BaseCmcicActions extends sfsPaymentActions
+{
+    protected $orderItem = null;
+    
+   /**
+    * Prepare data for charge and go to cmcic service.
+    *
+    * @param  void
+    * @return void
+    * @author Olivier Revollat <revollat@gmail.com>
+    * @access public
+    */
+    public function executeIndex($request)
+    {
+        sfLoader::loadHelpers(array('sfsCurrency', 'I18N'));
+        
+        $orderItem = $this->getOrderItemObjectByIdOrUuid($this->getRequestParameter('order_item_id'));
+        //$paymentService = $this->getPaymentServiceObject('cmcic');
+        //$params = sfsJSONPeer::decode($paymentService->getParams());
+        $currencyCode = $this->getUser()->getBasket()->getCurrency()->getCode();
+				
+        $cmcic_payment = new sfPaymentCIC();
+        $cmcic_payment->setLanguage(strtoupper($this->getUser()->getCulture())); 			// Language code 'FR, 'EN', ...
+        $cmcic_payment->setCurrency($currencyCode); 										// Currency code 'EUR'
+        $cmcic_payment->setAmount($orderItem->getTotalPrice());								// Order amount
+        $cmcic_payment->setOrderReference($orderItem->getId()); // $orderItem->getUuid()	// Order reference
+        
+        $this->form = new sfsCmcicChargeForm(); // Form to be post to the CM-CIC online payment website
+
+        $this->form->setDefaults(
+            array(
+				'version'			=> sfPaymentCIC::CMCIC_VERSION,
+				'TPE'				=> $cmcic_payment->getTPEData('number'),
+				'date'				=> sfPaymentCIC::HtmlEncode($cmcic_payment->getOrderDate()),
+				'montant'			=> $cmcic_payment->getAmount() . $currencyCode,
+				'reference'			=> $cmcic_payment->getOrderReference(),
+				'MAC'				=> $cmcic_payment->getKeyedMAC(),
+				'url_retour'		=> sfPaymentCIC::HtmlEncode(url_for('@order_paymentSuspend', true)),
+				'url_retour_ok'		=> sfPaymentCIC::HtmlEncode(url_for('@payment_cmcicCharged', true)),
+				'url_retour_err'	=> sfPaymentCIC::HtmlEncode(url_for('@payment_cmcicChargeFailed', true)),
+				'lgue'				=> strtoupper($this->getUser()->getCulture()),
+				'societe'			=> $cmcic_payment->getTPEData('company'),
+				'texte-libre'		=> ''
+            )
+        );
+                
+        $this->setLayout('layoutEmpty');
+    }
+    
+   /**
+    * Check charge.
+    * 
+    * @param  void
+    * @return void
+    * @author Olivier Revollat <revollat@gmail.com>
+    * @access public
+    */
+    public function executeCheckCharge($request)
+    {		
+    	$this->payment = new sfPaymentCIC();
+		$this->bank_response = $this->payment->checkResponse();
+		$isOK = ($this->bank_response['accuseReception'] == sfPaymentCIC::CMCIC_PHP2_MACOK);
+		
+		if($isOK){
+			$orderItem = OrderItemPeer::retrieveById($this->bank_response['reference']);
+			$this->setOrderStatusProcessing($orderItem->getUuid());	
+		}
+
+    	$response = $this->getResponse();
+  		$response->clearHttpHeaders();
+  		$response->setContent(sprintf(sfPaymentCIC::CMCIC_PHP2_RECEIPT, $this->bank_response['accuseReception']));
+  		$response->sendContent();
+  		return sfView::NONE;
+    }
+    
+   /**
+    * Success charge action.
+    * 
+    * @param  void
+    * @return void
+    * @author Dmitry Nesteruk <nesterukd@gmail.com>
+    * @access public
+    */
+    public function executeCharged($request)
+    {
+        $this->redirect('@order_checkoutFinished');
+    }
+    
+   /**
+    * Failed charge action.
+    * 
+    * @param  void
+    * @return void
+    * @author Dmitry Nesteruk <nesterukd@gmail.com>
+    * @access public
+    */
+    public function executeChargeFailed($request)
+    {
+        $this->redirect('@payment_chargeFailed');
+    }
+}
