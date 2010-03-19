3633be1f8ce8477c424c9ad6bac1609ee19981ec
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-03-19 16:31:34 +0000

    more cleanup performed

--- plugins/sfsCorePlugin/lib/user/sfsFrontendSecurityUser.class.php
+++ plugins/sfsCorePlugin/lib/user/sfsFrontendSecurityUser.class.php
@@ -1,5 +1,27 @@
 <?php
+/*
+ * This file is part of the sfShop package.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
 
+/**
+ * Class extends basic user class
+ *
+ * @package    sfShopPlugin
+ * @subpackage sfsCorePlugin
+ * @uses       sfGuardSecurityUser
+ * @author     Alexandru Emil Lupu <gang.alecs@gmail.com>
+ * @author     Andrey Kotlyarov
+ * @author     Dmitry Nesteruk 
+ * @license    http://www.opensource.org/licenses/mit-license.php
+ */
 class sfsFrontendSecurityUser extends sfGuardSecurityUser
 {
   
