70d5f99e366f5aec5f9d5169e3fd18eb06c01593
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-03-27 16:08:15 +0000

    Enabled the layout for the frontend as we need it.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -12,18 +12,18 @@
   </head>
   <body>
     <?php
-    //echo javascript_tag('
-    //    var status = {
-    //        ERROR: ' . sfsJSONPeer::STATUS_ERROR . ',
-    //        SUCCESS: ' . sfsJSONPeer::STATUS_SUCCESS . '
-    //    };
-    //');
+    echo javascript_tag('
+        var status = {
+            ERROR: ' . sfsJsonHandler::STATUS_ERROR . ',
+            SUCCESS: ' . sfsJsonHandler::STATUS_SUCCESS . '
+        };
+    ');
     ?>
     <div id="container_loading" style="display: none"><?php echo __('Loading') ?></div>
     <div class="body">
       <div>
         <div class="top_static_menu">
-          <?php include_component_slot('header');?>
+          <?php //include_component_slot('header');?>
           <?php include_component('sfsCore', 'selectLanguage'); ?>
           <?php include_component('sfsMenu', 'top'); ?>
         </div>
@@ -43,8 +43,8 @@
       <div class="head_container">
         <div class="logo_container">
           <div class="logo">
-            <?php //echo link_to(image_tag(sfConfig::get('app_sfshop_core_images_dir').'logo.gif', array('width' => 90, 'height' => 53, 'align' => 'absmiddle')), '@homepage') ?>
-            <?php //echo link_to('DVD shop','@homepage') ?>
+            <?php echo link_to(image_tag(sfConfig::get('app_sfshop_core_images_dir').'logo.gif', array('width' => 90, 'height' => 53, 'align' => 'absmiddle')), '@homepage') ?>
+            <?php echo link_to('DVD shop','@homepage') ?>
           </div>
         </div>
         <div class="login_container">
@@ -73,7 +73,7 @@
           <?php //include_component('currency', 'selectCurrencyForm'); ?>
         </div>
         <div class="column_center">
-          <?php //echo $sf_data->getRaw('sf_content') ?>
+          <?php echo $sf_content ?>
         </div>
       </div>
       <div class="menu_bottom">
@@ -85,7 +85,7 @@
       </div>
       <div class="copyright">Copyright 2010 &copy; <?php echo link_to('sfShop', 'http://code.google.com/p/sfshop/') ?></div>
 
-      <?php echo $sf_data->getRaw('sf_content') ?>
+      <?php echo $sf_content ?>
 
     </div>
   </body>
