d6c6580785f1e496eacfb98c62f87b1b02ebcf92
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-03-28 10:56:53 +0000

    Added error pages

---
+++ branches/Doctrine/plugins/sfsCorePlugin/modules/sfsCore/templates/error404Success.php
@@ -0,0 +1,3 @@
+<?php include_partial('sfsCore/container_header', array('caption' => __('Page not found', array(), 'sfsCorePlugin'))) ?>
+    <?php echo __('Sorry, but page which you requested was not found.', array(), 'sfsCorePlugin') ?>
+<?php include_partial('sfsCore/container_footer') ?>
---
+++ branches/Doctrine/plugins/sfsCorePlugin/modules/sfsCore/templates/error500Success.php
@@ -0,0 +1,3 @@
+<?php include_partial('sfsCore/container_header', array('caption' => __('Server internal error', array(), 'sfsCorePlugin'))) ?>
+    <?php echo __('Sorry, but something was broken, we has been notified about this error. As soon as possible we will fix this error and you will be able to use our service. ', array(), 'sfsCorePlugin') ?>
+<?php include_partial('sfsCore/container_footer') ?>
