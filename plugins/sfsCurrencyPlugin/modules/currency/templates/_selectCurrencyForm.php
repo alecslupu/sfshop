c9a3f19cbf65e0af59664ba633739b46af20baa0
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-04-19 10:33:21 +0000

    removed the sfsCurrencyPlugin

--- plugins/sfsCurrencyPlugin/modules/currency/templates/_selectCurrencyForm.php
+++
@@ -1,9 +0,0 @@
-<?php include_partial('core/box_header', array('caption' => __('Currency'))) ?>
-    <div class="currency_form">
-        <form action="<?php echo url_for('@currency_setSelected'); ?>" method="post" name="form_currency" class="form form_currency">
-            <ul class="main">
-                <?php echo $form ?>
-            </ul>
-        </form>
-    </div>
-<?php include_partial('core/box_footer') ?>
