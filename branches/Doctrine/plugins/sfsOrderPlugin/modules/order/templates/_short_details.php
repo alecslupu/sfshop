02bb2202888f024444930782eb9cf37ef9523de9
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-04-19 10:41:53 +0000

    removed sfsOrderPlugin

--- branches/Doctrine/plugins/sfsOrderPlugin/modules/order/templates/_short_details.php
+++
@@ -1,6 +0,0 @@
-<?php $i = 1; foreach ($itemProducts as $itemProduct): ?>
-    <?php echo $itemProduct->getProduct()->getTitle() ?>
-    <?php str_replace('<br/>', '\n', include_component('product', 'optionsList', array('itemProduct' => $itemProduct, 'method_for_get_options' => $method_for_get_options))) ?>
-<?php $i++; endforeach; ?>
-<?php echo __('Shipping') ?>: <?php echo format_currency($sf_user->getAttribute('price', null, 'order/delivery')) ?> 
-<?php echo __('Total') ?>: <?php echo format_currency($item->getTotalPrice()) ?>
