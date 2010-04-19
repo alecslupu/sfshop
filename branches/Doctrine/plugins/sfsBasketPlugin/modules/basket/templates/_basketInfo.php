096daa110325ec704d72f81ebab00450fddce0b7
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-04-19 10:34:46 +0000

    removed sfsBasketPlugin

--- branches/Doctrine/plugins/sfsBasketPlugin/modules/basket/templates/_basketInfo.php
+++
@@ -1,5 +0,0 @@
-<span class="header_basket_info" id="header_basket_info"></span>
-<?php echo javascript_tag('
-  var basketInfoUrl = "'.url_for('@basket_info', true).'";
-  basketInfoUrlUpdate();
-')?>
