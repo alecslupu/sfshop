4f0d98f217fc8b60c0294fec702a5a1d38c0dc82
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-04-19 10:32:29 +0000

    Added some extracode to the sfsCoreAdminPlugin plugin

---
+++ branches/Doctrine/plugins/sfsCoreAdminPlugin/data/template/layout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+
+    <?php include_javascripts()?>
+    <?php include_stylesheets()?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+  </head>
+  <body>
+    <div id="main">
+      <?php if ($sf_user->isAuthenticated()):?>
+        <?php include_component("sfsCoreAdminLayout","head"); ?>
+        <hr class="noscreen" />
+      <?php endif;  ?>
+       <?php echo $sf_content?>
+      <?php if ($sf_user->isAuthenticated()):?>
+        <hr class="noscreen" />
+        <?php include_component("sfsCoreAdminLayout","footer"); ?>
+      <?php endif;  ?>
+    </div>
+  </body>
+</html>
---
+++ branches/Doctrine/plugins/sfsCoreAdminPlugin/lib/routing/sfsCoreAdminPluginRouting.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class sfsCoreAdminPluginRouting
+{
+  public static function listenToSfsCoreAdminLayoutRoutingLoadConfigurationEvent(sfEvent $event)
+  {
+    
+  }
+}
