21c4bf9b46e788b204e61ad3a99da0f90c4ba9a4
gang.alecs <gang.alecs@e1a02840-6048-0410-8644-cfba31147e63>
2010-04-19 10:41:24 +0000

    removed sfsPaymentAuthorizeNetPlugin

--- plugins/sfsPaymentAuthorizeNetPlugin/modules/authorizeNet/templates/indexSuccess.php
+++
@@ -1,8 +0,0 @@
-<?php include_partial('core/container_header', array('caption' => __('Pay through AuthorizeNet'))) ?>
-    <form action="<?php echo url_for('@payment_authorizeNet?order_item_id=' . $sf_request->getParameter('order_item_id')) ?>" method="post" class="form">
-        <ul class="main">
-            <?php echo $form ?>
-            <li class="actions"><input type="submit" value="<?php echo __('Submit') ?>" class="button"></li>
-        </ul>
-    </form>
-<?php include_partial('core/container_footer') ?>
